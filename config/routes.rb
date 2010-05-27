476f63709d920eceb75a53a85cd8bd2b2aa0c189
Clinton R. Nixon <crnixon@gmail.com>
2010-05-27 17:37:43 -0400

    Added Typus

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  Typus::Routes.draw(map)
   Jammit::Routes.draw(map)
 
   # The priority is based upon order of creation: first created -> highest priority.
