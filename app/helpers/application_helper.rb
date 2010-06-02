37e0e59c97964f8b2c66893bf47d8cfa5b352981
Chris Jones <chris@yaychris.com>
2010-06-02 16:51:58 -0400

    Resolves #98 - community features page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def current_if(predicate)
+    'current' if predicate
+  end
 end
--- test/functional/communities_controller_test.rb
+++ test/functional/communities_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class CommunitiesControllerTest < ActionController::TestCase
-  context "a GET to #show" do
+  context "CommunitiesController" do
     setup do
       @community = Community.make
-      get :show, :id => @community.id
     end
 
-    should_assign_to(:community) { @community }
-    should_respond_with :success
-    should_render_template :show
+    %w(show features media neighborhood promotions contact).each do |action|
+      context "a GET to ##{action}" do
+        setup do
+          get action, :id => @community.id
+        end
+
+        should_assign_to(:community) { @community }
+        should_respond_with :success
+        should_render_template action
+      end
+    end
   end
 end
