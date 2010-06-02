b01178ae4807ae54e4b4615889b97ff49b719610
Chris Jones <chris@yaychris.com>
2010-06-02 16:23:26 -0400

    Routes & actions for community pages

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,5 +1,28 @@
 class CommunitiesController < ApplicationController
+  before_filter :find_community
+
   def show
-    @community = Community.find(params[:id])
   end
+
+  def features
+  end
+
+  def media
+  end
+
+  def neighborhood
+  end
+
+  def promotions
+  end
+
+  def contact
+  end
+
+
+  private
+
+    def find_community
+      @community = Community.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ ActionController::Routing::Routes.draw do |map|
   Typus::Routes.draw(map)
   Jammit::Routes.draw(map)
 
-  map.resources :communities, :only => [:show]
+  map.resources :communities,
+    :only => :show,
+    :member => {
+      :features     => :get,
+      :media        => :get,
+      :neighborhood => :get,
+      :promotions   => :get,
+      :contact      => :get
+    }
 end
