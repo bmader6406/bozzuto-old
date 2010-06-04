58d5db52a3c4a38d64b214ff54eee514dd23b12e
Chris Jones <chris@yaychris.com>
2010-06-04 00:19:37 -0400

    Show google map in sidebar of community page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def current_if(predicate)
     'current' if predicate
   end
+
+  def google_maps_javascript_tag
+    <<-END.html_safe
+<script src="http://maps.google.com/maps?file=api&v=2&key=#{APP_CONFIG[:google_maps_api_key]}" type="text/javascript"></script>
+    END
+  end
 end
