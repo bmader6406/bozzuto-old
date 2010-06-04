fe83672b6f18bdf2f0ae85f06dbbc231fbb676bf
Chris Jones <chris@yaychris.com>
2010-06-04 14:08:40 -0400

    Resolves #106 - see nearby communities

--- app/models/community.rb
+++ app/models/community.rb
@@ -5,11 +5,24 @@ class Community < ActiveRecord::Base
   has_many :floor_plans, :through => :floor_plan_groups
 
   validates_presence_of :title, :subtitle, :city
+  validates_numericality_of :latitude, :longitude, :allow_nil => true
 
   acts_as_mappable :lat_column_name => :latitude,
                    :lng_column_name => :longitude
 
   mount_uploader :promo_image, ImageUploader
+
+  named_scope :near, lambda { |loc|
+    returning({}) do |opts|
+      opts[:origin]     = loc
+      opts[:conditions] = ['id != ?', loc.id] if loc.is_a? Community
+      opts[:order]      = 'distance ASC'
+    end
+  }
+
+  def nearby_communities(limit = 6)
+    @nearby_communities ||= city.communities.near(self).all(:limit => limit)
+  end
 
   def address
     [street_address, city].compact.join(', ')
--- test/unit/community_test.rb
+++ test/unit/community_test.rb
@@ -8,6 +8,7 @@ class CommunityTest < ActiveSupport::TestCase
     should_have_many :floor_plans, :through => :floor_plan_groups
 
     should_validate_presence_of :title, :subtitle, :city
+    should_validate_numericality_of :latitude, :longitude
 
     context "#address" do
       setup do
@@ -17,6 +18,24 @@ class CommunityTest < ActiveSupport::TestCase
 
       should "return the formatted address" do
         assert_equal "#{@address}, #{@community.city}", @community.address
+      end
+    end
+
+    context "#nearby_communities" do
+      setup do
+        @city = City.make
+        @communities = []
+
+        3.times do |i|
+          @communities << Community.make(:latitude => i, :longitude => i, :city => @city)
+        end
+      end
+
+      should "return the closest communities" do
+        nearby = @communities[0].nearby_communities
+        assert_equal 2, nearby.length
+        assert_equal @communities[1], nearby[0]
+        assert_equal @communities[2], nearby[1]
       end
     end
   end
