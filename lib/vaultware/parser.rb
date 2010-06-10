4a631b41b7799be7a476b703f79ce8cd45feee9f
Chris Jones <chris@yaychris.com>
2010-06-10 10:12:45 -0400

    Vaultware::Parser - floor plan groups

--- lib/vaultware/parser.rb
+++ lib/vaultware/parser.rb
@@ -4,32 +4,43 @@ module Vaultware
 
     def parse(file)
       @data = Nokogiri::XML(File.read(file))
+    end
 
-      process_data
+    def process
+      data.xpath('/PhysicalProperty/Property', ns).each do |property|
+        process_property(property)
+        process_floor_plan_groups(property.xpath('./Floorplan'))
+      end
     end
 
 
     private
 
-      def process_data
-        data.xpath('/PhysicalProperty/Property', ns).each do |property|
-          process_property(property)
-        end
-      end
-
       def process_property(property)
         info = property.at('./PropertyID/ns:Identification', ns)
         address = property.at('./PropertyID/ns:Address', ns)
 
-        community = Community.find_or_initialize_by_vaultware_id(
+        @community = Community.find_or_initialize_by_vaultware_id(
           info.at('./ns:PrimaryID', ns).content.to_i
         )
-        community.update_attributes({
+        @community.update_attributes({
           :title          => info.at('./ns:MarketingName', ns).content,
           :website_url    => info.at('./ns:WebSite', ns).content,
           :street_address => address.at('./ns:Address1', ns).content,
           :city           => find_city(address)
         })
+      end
+
+      def process_floor_plan_groups(plans)
+        @community.floor_plan_groups.destroy_all
+
+        groups = plans.map do |group|
+          group.at('./Comment').content
+        end.uniq.sort
+
+        groups.each do |group|
+          @community.floor_plan_groups << FloorPlanGroup.new(:name => group)
+        end
       end
 
       def find_city(address)
--- test/unit/vaultware/parser_test.rb
+++ test/unit/vaultware/parser_test.rb
@@ -10,6 +10,7 @@ class ParserTest < ActiveSupport::TestCase
       @fixture    = load_fixture_file('vaultware.xml')
       @data       = Nokogiri::XML(@fixture)
       @properties = @data.xpath('/PhysicalProperty/Property')
+      @property   = @properties.first
 
       @parser = Vaultware::Parser.new
     end
@@ -17,43 +18,80 @@ class ParserTest < ActiveSupport::TestCase
     context '#parse' do
       setup do
         @file = RAILS_ROOT + '/test/fixtures/vaultware.xml'
+        @parser.parse(@file)
       end
 
       should 'load the file' do
-        @parser.parse(@file)
+        @parser.process
         assert @parser.data
         assert @parser.data.xpath('/PhysicalProperty').present?
       end
 
       should 'create the communities' do
         assert_difference('Community.count', @properties.count) do
-          @parser.parse(@file)
+          @parser.process
         end
       end
 
       context 'a community already exists with a vaultware id' do
         setup do
-          @property = @properties.first
-          @vaultware_id = @property.at('./PropertyID/ns:Identification/ns:PrimaryID', ns).content.to_i
+          @vaultware_id = vaultware_id(@property)
           @community = Community.make :vaultware_id => @vaultware_id
         end
 
         should 'update the existing community' do
           assert_difference('Community.count', @properties.count - 1) do
-            @parser.parse(@file)
+            @parser.process
           end
 
           @community.reload
 
-          assert_equal @property.at('./PropertyID/ns:Identification/ns:MarketingName', ns).content,
-            @community.title
+          assert_equal title(@property), @community.title
+        end
+      end
+
+      context 'processing floor plan groups' do
+        setup do
+          @groups = @property.xpath('Floorplan').map { |plan|
+            plan.at('./Comment').try(:content)
+          }.compact.uniq.sort
+
+          @community = Community.make :vaultware_id => vaultware_id(@property)
+        end
+
+        should 'create the floor plan groups' do
+          @parser.process
+          @community.reload
+
+          assert_equal @groups.count, @community.floor_plan_groups.count
+
+          @community.floor_plan_groups.each_with_index do |group, i|
+            assert_equal @groups[i], group.name
+          end
+        end
+
+        should 'destroy any existing floor plan groups' do
+          group = FloorPlanGroup.make
+          @community.floor_plan_groups << group
+          @parser.process
+
+          assert_nil @community.floor_plan_groups.find_by_name(group.name)
         end
       end
     end
   end
 
+
   def ns
     { 'ns' => 'http://my-company.com/namespace' }
+  end
+
+  def vaultware_id(property)
+    property.at('./PropertyID/ns:Identification/ns:PrimaryID', ns).content.to_i
+  end
+
+  def title(property)
+    property.at('./PropertyID/ns:Identification/ns:MarketingName', ns).content
   end
 end
 
