e22e86f16ad21c916036ca85d7c4332045ef8a3c
Chris Jones <chris@yaychris.com>
2010-06-11 16:28:34 -0400

    Vaultware::Parser - test for the actual fields

--- test/unit/vaultware/parser_test.rb
+++ test/unit/vaultware/parser_test.rb
@@ -31,6 +31,13 @@ class ParserTest < ActiveSupport::TestCase
         assert_difference('Community.count', @properties.count) do
           @parser.process
         end
+
+        community = Community.find_by_vaultware_id(vaultware_id(@property))
+        attrs = community_attributes(@property)
+
+        community_fields.each do |field|
+          assert_equal attrs[field], community.send(field)
+        end
       end
 
       context 'a community already exists with a vaultware id' do
@@ -88,6 +95,14 @@ class ParserTest < ActiveSupport::TestCase
 
         should 'create the floor plans' do
           assert_equal @plans.count, @community.floor_plans.count
+
+          @community.floor_plans.each_with_index do |plan, i|
+            attrs = floor_plan_attributes(@plans[i])
+
+            floor_plan_fields.each do |field|
+              assert_equal attrs[field], plan.send(field)
+            end
+          end
         end
       end
     end
@@ -104,6 +119,62 @@ class ParserTest < ActiveSupport::TestCase
 
   def title(property)
     property.at('./PropertyID/ns:Identification/ns:MarketingName', ns).content
+  end
+
+
+  def community_fields
+    [
+      :title,
+      :website_url,
+      :street_address,
+      :availability_url
+    ]
+  end
+
+  def community_attributes(property)
+    ident   = property.at('./PropertyID/ns:Identification', ns)
+    address = property.at('./PropertyID/ns:Address', ns)
+    info    = property.at('./Information')
+
+    {
+      :title            => ident.at('./ns:MarketingName', ns).content,
+      :website_url      => ident.at('./ns:WebSite', ns).try(:content),
+      :street_address   => address.at('./ns:Address1', ns).content,
+      :availability_url => info.at('./PropertyAvailabilityURL').content
+    }
+  end
+
+
+  def floor_plan_fields
+    [
+      :name,
+      :availability_url,
+      :bedrooms,
+      :bathrooms,
+      :min_square_feet,
+      :max_square_feet,
+      :min_market_rent,
+      :max_market_rent,
+      :min_effective_rent,
+      :max_effective_rent,
+      :image
+    ]
+  end
+
+  def floor_plan_attributes(plan)
+    {
+      :name               => plan.at('./Name').content,
+      :availability_url   => plan.at('./FloorplanAvailabilityURL').content,
+      :bedrooms           => plan.at('./Room[@Type="Bedroom"]/Count').content.to_i,
+      :bathrooms          => plan.at('./Room[@Type="Bathroom"]/Count').content.to_f,
+      :min_square_feet    => plan.at('./SquareFeet')['Min'].to_i,
+      :max_square_feet    => plan.at('./SquareFeet')['Max'].to_i,
+      :min_market_rent    => plan.at('./MarketRent')['Min'].to_f,
+      :max_market_rent    => plan.at('./MarketRent')['Max'].to_f,
+      :min_effective_rent => plan.at('./EffectiveRent')['Min'].to_f,
+      :max_effective_rent => plan.at('./EffectiveRent')['Max'].to_f,
+      :image              => plan.at('./File/Src').try(:content)
+    }
   end
 end
 
