ce68b218931de824982769fcc8ec24f942c3be27
Chris Jones <chris@yaychris.com>
2010-06-22 10:58:18 -0400

    Full test coverage for County

--- test/unit/county_test.rb
+++ test/unit/county_test.rb
@@ -14,5 +14,11 @@ class CountyTest < ActiveSupport::TestCase
 
     should_validate_presence_of :name, :state
     should_validate_uniqueness_of :name, :scoped_to => :state_id
+
+    context '#to_s' do
+      should 'return the county name and state' do
+        assert_equal "#{@county.name}, #{@county.state.code}", @county.to_s
+      end
+    end
   end
 end
