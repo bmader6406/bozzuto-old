e22bfbcdb900a1f6fb4233318cefdd94c8cae806
Chris Jones <chris@yaychris.com>
2010-06-27 22:37:37 -0400

    Resolves #140 - link to topic-specific contact form from project

--- app/helpers/contact_submissions_helper.rb
+++ app/helpers/contact_submissions_helper.rb
@@ -1,2 +1,10 @@
 module ContactSubmissionsHelper
+  def contact_path_with_topic
+    topic = if @section.try(:service?)
+      @section.cached_slug.gsub(/-/, '_')
+    else
+      'general_inquiry'
+    end
+    contact_path(:topic => topic)
+  end
 end
--- test/unit/helpers/contact_submissions_helper_test.rb
+++ test/unit/helpers/contact_submissions_helper_test.rb
@@ -1,4 +1,29 @@
 require 'test_helper'
 
-class ContactsHelperTest < ActionView::TestCase
+class ContactSubmissionsHelperTest < ActionView::TestCase
+  context 'ContactSubmissionsHlper' do
+    context '#contact_path_with_topic' do
+      context 'when section is a service' do
+        setup do
+          @section = Section.make(:service, :title => 'Construction')
+        end
+
+        should 'set the topic to service' do
+          assert_equal contact_path(:topic => 'construction'),
+            contact_path_with_topic
+        end
+      end
+
+      context 'when section is not a service' do
+        setup do
+          @section = Section.make
+        end
+
+        should 'set the topic to general_inquiry' do
+          assert_equal contact_path(:topic => 'general_inquiry'),
+            contact_path_with_topic
+        end
+      end
+    end
+  end
 end
