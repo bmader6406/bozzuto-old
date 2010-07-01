bf6d4ba81c93be713f852806fae754a80543d810
Chris Jones <chris@yaychris.com>
2010-07-01 07:52:55 -0400

    Show a mini slideshow on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @latest_news = NewsPost.published.latest(1).first
+    @property = PropertyMiniSlideshow.first.try(:property)
   end
 end
