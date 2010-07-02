7ea402c1e37fa8a2a6f77dc6ab283009f99969bc
Chris Jones <chris@yaychris.com>
2010-07-02 15:46:51 -0400

    HomePage and HomePageSlides controllers for Typus
    - home page controller directs straight to edit page
    - custom view for home page slides form to hide relationship

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -2,7 +2,8 @@ class HomePagesController < ApplicationController
   layout 'homepage'
 
   def index
+    @home_page = HomePage.first
     @latest_news = NewsPost.published.latest(1).first
-    @property = PropertyMiniSlideshow.first.try(:property)
+    @property = @home_page.featured_property
   end
 end
