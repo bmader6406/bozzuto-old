7ed7f9dfcbd09bca09ec6ab99c86b7a687cf4f5a
Chris Jones <chris@yaychris.com>
2010-07-10 19:25:54 -0400

    Test for CommunityMediaController

--- test/functional/community_media_controller_test.rb
+++ test/functional/community_media_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class CommunityMediaControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context 'CommunityMediaController' do
+    context 'a GET to #index' do
+      setup do
+        @community = ApartmentCommunity.make
+        get :index, :apartment_community_id => @community.id
+      end
+
+      should_respond_with :success
+      should_render_template :index
+      should_assign_to(:community) { @community }
+    end
   end
 end
