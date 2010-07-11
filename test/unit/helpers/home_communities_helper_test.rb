1fb2abb2bbab6dbabe05d04bea9055989449a09b
Chris Jones <chris@yaychris.com>
2010-07-11 16:15:26 -0400

    Resolves #205 - add mortgage calculator to home community page

--- test/unit/helpers/home_communities_helper_test.rb
+++ test/unit/helpers/home_communities_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class HomeCommunitiesHelperTest < ActionView::TestCase
+  context '#zillow_mortgage_calculator' do
+    should 'return the Zillow code' do
+      assert_match /Zillow/, zillow_mortgage_calculator
+    end
+
+    should 'be marked html_safe' do
+      assert zillow_mortgage_calculator.html_safe?
+    end
+  end
 end
