9dbabd07b67aaa0a9d593c85b1be684bf4b4d39a
Chris Jones <chris@yaychris.com>
2010-07-13 16:40:27 -0400

    Fix issue with PropertyFeature associations

--- app/models/property_feature.rb
+++ app/models/property_feature.rb
@@ -3,9 +3,11 @@ class PropertyFeature < ActiveRecord::Base
 
   has_and_belongs_to_many :properties
   has_and_belongs_to_many :apartment_communities,
-    :join_table => 'properties_property_features'
+    :join_table => 'properties_property_features',
+    :association_foreign_key => :property_id
   has_and_belongs_to_many :home_communities,
-    :join_table => 'properties_property_features'
+    :join_table => 'properties_property_features',
+    :association_foreign_key => :property_id
 
   has_attached_file :icon,
     :url => '/system/:class/:id/icon_:style.:extension',
