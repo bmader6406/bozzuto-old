5fe2bf19d446ff2ced0027b75b398002de11923a
Chris Jones <chris@yaychris.com>
2010-07-15 22:10:01 -0400

    Update seed file with Services section

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
-
+###
+# States
 State.create([{ :code => 'CT', :name => 'Connecticut' },
               { :code => 'MD', :name => 'Maryland' },
               { :code => 'MA', :name => 'Massachusetts' },
@@ -15,13 +9,22 @@ State.create([{ :code => 'CT', :name => 'Connecticut' },
               { :code => 'VA', :name => 'Virginia' },
               { :code => 'DC', :name => 'Washington, DC' }])
 
+###
+# Sections
+
+# services
 %w(Acquisitions Construction Development Homebuilding Land Management).each do |title|
   section = Section.create(:title => title, :service => true)
 end
-Section.create(:title => 'About')
-Section.create(:title => 'Apartments')
-Section.create(:title => 'New Homes')
 
+# sections
+['About', 'Apartments', 'New Homes', 'Services'].each do |section|
+  Section.create(:title => section)
+end
+
+
+###
+# Apartment Floor Plan Groups
 ['Studio', '1 Bedroom', '2 Bedrooms', '3 or More Bedrooms', 'Penthouse'].each do |group|
   ApartmentFloorPlanGroup.find_or_create_by_name(group)
 end
@@ -33,14 +36,16 @@ end
   PhotoGroup.find_or_create_by_title(attrs)
 end
 
+
+###
+# Create a default home page
 if HomePage.count.zero?
   HomePage.new.save(false)
 end
 
+
+###
+# Seed with XML data
 parser = Vaultware::Parser.new
 parser.parse(RAILS_ROOT + '/db/seeds/vaultware.xml')
 parser.process
-
-['Studio', '1 Bedroom', '2 Bedrooms', '3 or More Bedrooms', 'Penthouse'].each do |name|
-  ApartmentFloorPlanGroup.find_or_create_by_name(name)
-end
