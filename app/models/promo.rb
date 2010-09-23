f3a8e2dab2a729b228a577a6f0b4af120e3f57b1
Chris Jones <chris@yaychris.com>
2010-09-23 16:54:46 -0400

    Add expiration date fields to Promo, U#431

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -4,8 +4,19 @@ class Promo < ActiveRecord::Base
   has_many :landing_pages
 
   validates_presence_of :title, :subtitle
+  validates_presence_of :expiration_date, :if => proc { |record| record.has_expiration_date? }
+  validates_inclusion_of :has_expiration_date, :in => [true, false]
+
+  before_validation :nullify_expiration_date
 
   def typus_name
     title
+  end
+
+
+  private
+
+  def nullify_expiration_date
+    write_attribute(:expiration_date, nil) unless has_expiration_date?
   end
 end
--- test/unit/promo_test.rb
+++ test/unit/promo_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class PromoTest < ActiveSupport::TestCase
   context 'Promo' do
+    setup { @promo = Promo.new }
+    subject { @promo }
+
     should_have_many :apartment_communities,
       :home_communities,
       :landing_pages
@@ -9,13 +12,39 @@ class PromoTest < ActiveSupport::TestCase
     should_validate_presence_of :title, :subtitle
 
     context '#typus_name' do
-      setup do
-        @promo = Promo.new :title => 'Hey ya'
-      end
+      setup { @promo.title = 'Hey ya' }
 
       should 'return the title' do
         assert_equal 'Hey ya', @promo.typus_name
       end
+    end
+
+    context 'has_expiration_date is false' do
+      setup do
+        @promo.has_expiration_date = false
+      end
+
+      should 'set expiration_date to nil before validation' do
+        @promo.expiration_date = Time.now
+        assert @promo.expiration_date.present?
+        @promo.valid?
+        assert_nil @promo.expiration_date
+      end
+
+      should 'allow nil for expiration_date' do
+        @promo.expiration_date = nil
+        @promo.valid?
+        assert_nil @promo.errors.on(:expiration_date)
+      end
+    end
+
+    context 'has_expiration_date is true' do
+      setup do
+        @promo.has_expiration_date = true
+        @promo.expiration_date = nil
+      end
+
+      should_validate_presence_of :expiration_date
     end
   end
 end
