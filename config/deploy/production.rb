93953139699dc9c1a3c8402759d5572fd08e40bf
Chris Jones <chris@yaychris.com>
2010-11-02 11:23:49 -0400

    Set production deploy path to include REE

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,8 @@ set :user, 'apache'
 set :deploy_to,  "/var/www/#{application}"
 set :branch, "origin/production"
 
+set :default_environment, { 'PATH' => '/opt/ruby-enterprise-1.8.7-2010.02/bin:$PATH' }
+
 role :web, "bozzuto.com"
 role :app, "bozzuto.com"
 role :db,  "bozzuto.com", :primary => true
