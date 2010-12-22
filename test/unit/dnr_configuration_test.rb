a586ad92ed93aab9fdaf5edeb6db41c7efc95839
Chris Jones <chris@yaychris.com>
2010-12-22 09:08:37 -0500

    DNR configuration typus controller

--- test/unit/dnr_configuration_test.rb
+++ test/unit/dnr_configuration_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class DnrConfigurationTest < ActiveSupport::TestCase
   context 'A DNR configuration' do
     should_belong_to :property
-    should_validate_presence_of :customer_code
+    should_validate_presence_of :customer_code, :property
   end
 end
