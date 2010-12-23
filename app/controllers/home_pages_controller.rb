078fac42a31cd11d66df295c42080f6c97acd085
Chris Jones <chris@yaychris.com>
2010-12-23 11:39:34 -0500

    Add mobile view for home_page#index

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,8 +1,15 @@
 class HomePagesController < ApplicationController
-  layout 'homepage'
+  layout :detect_mobile_layout
 
   def index
     @home_page = HomePage.first
     @latest_news = NewsPost.published.latest(1).first
+  end
+
+
+  private
+
+  def detect_mobile_layout
+    mobile? ? 'application' : 'homepage'
   end
 end
