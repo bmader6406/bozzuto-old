8ddaa9ea26660397ebe6ab5f75c7156e4e03e44a
Chris Jones <chris@yaychris.com>
2010-12-30 10:55:32 -0500

    Mobile tests for home_pages#index

--- test/functional/home_pages_controller_test.rb
+++ test/functional/home_pages_controller_test.rb
@@ -4,19 +4,35 @@ class HomePagesControllerTest < ActionController::TestCase
   context 'HomePagesController' do
     context 'a GET to #index' do
       setup do
-        3.times { |i| NewsPost.make :published_at => (Time.now - i.days) }
-        @post = NewsPost.published.latest(1).first
-
         @home_page = HomePage.new
         @home_page.save(false)
-
-        get :index
       end
 
-      should_respond_with :success
-      should_render_template :index
-      should_assign_to(:home_page) { @home_page }
-      should_assign_to(:latest_news) { @post }
+      browser_context do
+        setup do
+          3.times { |i| NewsPost.make :published_at => (Time.now - i.days) }
+          @post = NewsPost.published.latest(1).first
+
+          get :index
+        end
+
+        should_respond_with :success
+        should_render_with_layout :homepage
+        should_render_template :index
+        should_assign_to(:home_page) { @home_page }
+        should_assign_to(:latest_news) { @post }
+      end
+
+      mobile_context do
+        setup do
+          get :index, :format => :mobile
+        end
+
+        should_respond_with :success
+        should_render_with_layout :application
+        should_render_template :index
+        should_assign_to(:home_page) { @home_page }
+      end
     end
   end
 end
