262342b191eeec3770c3017903a6f89ca5b43562
Chris Jones <chris@yaychris.com>
2011-01-18 14:09:11 -0500

    Mobile view for homes#index, updates #504

--- test/unit/helpers/homes_helper_test.rb
+++ test/unit/helpers/homes_helper_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class HomesHelperTest < ActionView::TestCase
+  context '#render_homes_mobile_listings' do
+    setup do
+      @community = HomeCommunity.make
+      @home      = Home.make(:home_community => @community)
+    end
+
+    should 'render the partial with the correct options' do
+      expects(:render).with({
+        :partial    => 'homes/listing',
+        :locals     => {
+          :community => @community,
+          :home      => @home
+        }
+      })
+
+      render_homes_mobile_listings(@community)
+    end
+  end
 end
