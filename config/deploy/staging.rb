a63c6ee3f08940fe4fe3d28c4eb1729405ff7735
Chris Jones <chris@yaychris.com>
2011-03-04 11:14:45 -0500

    Set deploy PATH for integration and staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,8 @@ set :user, 'apache'
 set :deploy_to,  "/var/www/html/#{application}/staging"
 set :branch, 'origin/staging'
 
+set :default_environment, { 'PATH' => '/opt/ruby-enterprise-1.8.7-2011.03/bin:$PATH' }
+
 role :web, "pottsville.lab.viget.com"
 role :app, "pottsville.lab.viget.com"
 role :db,  "pottsville.lab.viget.com", :primary => true
