1f6dcbe19f9442d837eccde36682b984a0090455
Chris Jones <chris@yaychris.com>
2011-03-07 13:38:09 -0500

    Use '/opt/ree' symlink when deploying

--- config/deploy/integration.rb
+++ config/deploy/integration.rb
@@ -2,7 +2,7 @@ set :user, 'apache'
 set :deploy_to,  "/var/www/html/#{application}/integration"
 set :branch, 'origin/master'
 
-set :default_environment, { 'PATH' => '/opt/ruby-enterprise-1.8.7-2011.03/bin:$PATH' }
+set :default_environment, { 'PATH' => '/opt/ree/bin:$PATH' }
 
 role :web, "pottsville.lab.viget.com"
 role :app, "pottsville.lab.viget.com"
