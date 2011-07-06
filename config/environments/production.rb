635799408cb2559b53a6bc3041c7c251adffe979
Chris Jones <chris@yaychris.com>
2011-07-06 12:00:00 -0400

    Use temporary bozzutoinquiries@vigetlabs.com email address in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,13 +33,22 @@ config.action_mailer.default_url_options = {
   :host => 'bozzuto.com'
 }
 
-config.action_mailer.smtp_settings = {
-  :address              => 'mail.bozzuto.com',
-  :domain               => 'bozzuto.com',
-  :port                 => 25,
-  :user_name            => 'inquiries',
-  :password             => 'Bozzuto22',
-  :authentication       => :login,
-  :enable_starttls_auto => false
-}
+#config.action_mailer.smtp_settings = {
+#  :address              => 'mail.bozzuto.com',
+#  :domain               => 'bozzuto.com',
+#  :port                 => 25,
+#  :user_name            => 'inquiries',
+#  :password             => 'Bozzuto22',
+#  :authentication       => :login,
+#  :enable_starttls_auto => false
+#}
 
+config.action_mailer.smtp_settings = {
+  :address              => 'smtp.gmail.com',
+  :domain               => 'vigetlabs.com',
+  :port                 => 587,
+  :user_name            => 'bozzutoinquiries@vigetlabs.com',
+  :password             => 'SbR2bZ>D',
+  :authentication       => :plain,
+  :enable_starttls_auto => true
+}
