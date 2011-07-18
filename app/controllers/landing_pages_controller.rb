65cae845725b809fd9ed8cab3af3ad450a7bb55a
Chris Jones <chris@yaychris.com>
2011-07-18 12:03:13 -0400

    Redirect non-canonical landing page URLs, resolves #590

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -3,8 +3,23 @@ class LandingPagesController < ApplicationController
 
   layout 'homepage'
 
+  before_filter :find_page, :only => :show
+  before_filter :redirect_to_canonical_url, :only => :show
+
   def show
-    @page = LandingPage.published.find(params[:id])
     @state = @page.state
+  end
+
+
+  private
+
+  def find_page
+    @page = LandingPage.published.find(params[:id])
+  end
+
+  def redirect_to_canonical_url
+    if request.path != landing_page_path(@page)
+      redirect_to @page, :status => :moved_permanently
+    end
   end
 end
--- test/functional/landing_pages_controller_test.rb
+++ test/functional/landing_pages_controller_test.rb
@@ -2,24 +2,62 @@ require 'test_helper'
 
 class LandingPagesControllerTest < ActionController::TestCase
   context 'LandingPagesController' do
+    setup do
+      @page      = LandingPage.make
+      @city      = City.make(:state => @page.state)
+      @community = ApartmentCommunity.make(:city => @city)
+    end
+
     context 'a GET to #show' do
       %w(browser mobile).each do |device|
         send("#{device}_context") do
           setup do
             set_mobile_user_agent! if device == 'mobile'
 
-            @page      = LandingPage.make
-            @city      = City.make(:state => @page.state)
-            @community = ApartmentCommunity.make(:city => @city)
-
             get :show, :id => @page.to_param
           end
 
           should_respond_with :success
+          should_render_with_layout :homepage
           should_render_template :show
           should_assign_to(:page) { @page }
           should_assign_to(:state) { @page.state }
         end
+      end
+    end
+
+    context 'with a canonical URL' do
+      setup do
+        get :show, :id => @page.to_param
+      end
+
+      should_respond_with :success
+      should_render_template :show
+      should_assign_to(:page) { @page }
+      should_assign_to(:state) { @page.state }
+    end
+
+    context 'with a non-canonical URL' do
+      context 'that uses a slug' do
+        setup do
+          old_slug = @page.cached_slug
+
+          @page.update_attributes(:title => 'Hooray')
+
+          get :show, :id => old_slug
+        end
+
+        should_respond_with :redirect
+        should_redirect_to('the canonical URL') { landing_page_url(@page) }
+      end
+
+      context 'that uses an ID number' do
+        setup do
+          get :show, :id => @page.id
+        end
+
+        should_respond_with :redirect
+        should_redirect_to('the canonical URL') { landing_page_url(@page) }
       end
     end
   end
