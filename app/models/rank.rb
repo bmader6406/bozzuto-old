163b6e1cbbfe05509a895feab375ed909897a967
Chris Jones <chris@yaychris.com>
2011-10-25 12:57:04 -0400

    Allow arbitrarily large rank number support, fixes #620

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,8 +1,11 @@
 class Rank < ActiveRecord::Base
-  YEAR        = (1988..(Time.now.year + 1)).to_a.reverse.map(&:to_s)
-  RANK_NUMBER = (1..50).to_a.map(&:to_s)
+  YEAR = (1988..(Time.now.year + 1)).to_a.reverse
 
   validates_presence_of :rank_number, :year
+
+  validates_numericality_of :rank_number,
+    :only_integer => true,
+    :greater_than => 0
 
   belongs_to :rank_category
 
--- test/unit/rank_test.rb
+++ test/unit/rank_test.rb
@@ -5,5 +5,7 @@ class RankTest < ActiveSupport::TestCase
 
   should_validate_presence_of :rank_number, :year
 
+  should_validate_numericality_of :rank_number
+
   should_belong_to :rank_category
 end
