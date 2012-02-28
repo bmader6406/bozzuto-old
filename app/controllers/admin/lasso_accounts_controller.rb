8282a6829af4cb35401f7898970a6f02d005c487
Chris Jones <chris@yaychris.com>
2012-02-28 14:40:48 -0500

    Fix bug in Admin::LassoAccountsController

--- app/controllers/admin/lasso_accounts_controller.rb
+++ app/controllers/admin/lasso_accounts_controller.rb
@@ -1,3 +1,16 @@
 class Admin::LassoAccountsController < Admin::MasterController
-  
+  def create_with_back_to
+    resource_class = params[:resource].classify.constantize
+    resource_id = params[:resource_id]
+    resource = resource_class.find(resource_id)
+
+    @item.property = resource
+    @item.save
+
+    flash[:success] = _("{{model_a}} successfully assigned to {{model_b}}.",
+                        :model_a => @item.class.typus_human_name,
+                        :model_b => resource_class.typus_human_name)
+
+    redirect_to params[:back_to]
+  end
 end
