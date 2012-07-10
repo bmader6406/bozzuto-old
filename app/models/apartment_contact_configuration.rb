1e205745dd10eb71da55880cc925ca03af6febd9
Chris Jones <chris@yaychris.com>
2012-07-10 15:47:21 -0400

    Default upcoming_intro_text to ApartmentContactConfiguration, fixes #733

--- app/models/apartment_contact_configuration.rb
+++ app/models/apartment_contact_configuration.rb
@@ -2,4 +2,10 @@ class ApartmentContactConfiguration < ActiveRecord::Base
   belongs_to :apartment_community
 
   validates_presence_of :apartment_community
+
+  def after_initialize
+    if upcoming_intro_text.blank?
+      self.upcoming_intro_text = 'Sign up below to be the first to know when this community is read to accept residents.'
+    end
+  end
 end
