e05351a2fd2d5e1b267e343ac42a2cfac406e160
Zachary Porter <zporter9@gmail.com>
2012-08-29 16:27:45 -0400

    Fixes string interpolation on tracking code
    Refs #755

--- app/helpers/tracking_code_helper.rb
+++ app/helpers/tracking_code_helper.rb
@@ -2,7 +2,7 @@ module TrackingCodeHelper
   def value_click_tracking_code
     <<-END.html_safe
       <!-- ValueClick -->
-      <img src="http://media.fastclick.net/w/tre?ad_id=27340;evt=20270;cat1=26729;cat2=26730;rand=<%= "#{Time.now.strftime('%Y%m%d%H%M%S')}#{SecureRandom.hex(2)}" %>" width="1" height="1" border="0">
+      <img src="http://media.fastclick.net/w/tre?ad_id=27340;evt=20270;cat1=26729;cat2=26730;rand=#{Time.now.strftime('%Y%m%d%H%M%S')}#{SecureRandom.hex(2)}" width="1" height="1" border="0">
       <!-- End ValueClick -->
     END
   end
