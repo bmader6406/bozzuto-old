0d18690ad180accef66935fcd8a51d82dfaa8034
Zachary Porter <zporter9@gmail.com>
2012-09-17 16:33:18 -0400

    Replaces SecureRandom for Kernel.rand
    Until uuidtools gets added to production Gemfile
    
    Refs #755

--- app/helpers/tracking_code_helper.rb
+++ app/helpers/tracking_code_helper.rb
@@ -2,8 +2,14 @@ module TrackingCodeHelper
   def value_click_tracking_code
     <<-END.html_safe
       <!-- ValueClick -->
-      <img src="http://media.fastclick.net/w/tre?ad_id=27340;evt=20270;cat1=26729;cat2=26730;rand=#{Time.now.strftime('%Y%m%d%H%M%S')}#{SecureRandom.hex(2)}" width="1" height="1" border="0">
+      <img src="http://media.fastclick.net/w/tre?ad_id=27340;evt=20270;cat1=26729;cat2=26730;rand=#{Time.now.strftime('%Y%m%d%H%M%S')}#{random_number_string(4)}" width="1" height="1" border="0">
       <!-- End ValueClick -->
     END
+  end
+
+  private
+
+  def random_number_string(length)
+    "%0#{length}d" % (Kernel.rand * (10 ^ length))
   end
 end
