7fd435284fa394cf19103e0cb046b7b2fa45eedd
Zachary Porter <zporter9@gmail.com>
2012-09-23 11:53:29 -0400

    Updates db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2012081313174438) do
+ActiveRecord::Schema.define(:version => 20120921144045) do
 
   create_table "apartment_communities_landing_pages", :id => false, :force => true do |t|
     t.integer "landing_page_id"
