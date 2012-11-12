f73d9f753a89c42a5f0a419aa1322412aa5b0a17
Zachary Porter <zporter9@gmail.com>
2012-11-12 14:32:57 -0500

    Fix Apt Floor Plan observer where community is nil
    There was a bug when reprocessing images on ApartmentFloorPlans.
    Some floor plans did not have a community set (perhaps orphaned data?).
    This fix addresses that within the observer class

--- app/models/apartment_floor_plan_observer.rb
+++ app/models/apartment_floor_plan_observer.rb
@@ -15,16 +15,22 @@ class ApartmentFloorPlanObserver < ActiveRecord::Observer
   def cache_cheapest_price_on_community(plan)
     group          = plan.floor_plan_group
     community      = plan.apartment_community
-    cheapest_price = community.floor_plans.in_group(group).non_zero_min_rent.minimum(:min_rent)
 
-    community.update_attributes("cheapest_#{group.name_for_cache}_price" => cheapest_price)
+    if community
+      cheapest_price = community.floor_plans.in_group(group).non_zero_min_rent.minimum(:min_rent)
+
+      community.update_attributes("cheapest_#{group.name_for_cache}_price" => cheapest_price)
+    end
   end
 
   def cache_plan_count_on_community(plan)
     group     = plan.floor_plan_group
     community = plan.apartment_community
-    count     = community.floor_plans.in_group(group).count
 
-    community.update_attributes("plan_count_#{group.name_for_cache}" => count)
+    if community
+      count     = community.floor_plans.in_group(group).count
+
+      community.update_attributes("plan_count_#{group.name_for_cache}" => count)
+    end
   end
 end
