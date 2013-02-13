2b13ea1fe69df7c8d693af35eb3ced65bb3bfe40
Chris Jones <chris@yaychris.com>
2013-02-13 16:29:27 -0500

    Careers mobile page, resolves #822

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,10 +1,15 @@
 class CareersController < SectionContentController
+  has_mobile_actions :index
+
   before_filter :find_section
 
   def index
     @page    = @section.pages.published.first
     @entries = CareersEntry.all(:limit => 4)
 
-    render :index, :layout => 'application'
+    respond_to do |wants|
+      wants.html   { render :index, :layout => 'application' }
+      wants.mobile { render 'pages/show' }
+    end
   end
 end
--- test/functional/careers_controller_test.rb
+++ test/functional/careers_controller_test.rb
@@ -2,14 +2,32 @@ require 'test_helper'
 
 class CareersControllerTest < ActionController::TestCase
   context 'GET to :index' do
-    setup do
-      @section = Section.make :title => 'Careers'
+    browser_context do
+      setup do
+        @section = Section.make :title => 'Careers'
 
-      get :index, :section => 'careers'
+        get :index, :section => 'careers'
+      end
+
+      should_respond_with :success
+      should_render_template :index
+      should_assign_to(:section) { @section }
     end
 
-    should_respond_with :success
-    should_render_template :index
-    should_assign_to(:section) { @section }
+    mobile_context do
+      setup do
+        @section = Section.make :title => 'Careers'
+        @page    = Page.make :section => @section
+
+        set_mobile_user_agent!
+        get :index, :section => 'careers',
+                    :format  => :mobile
+      end
+
+      should_respond_with :success
+      should_render_template 'pages/show.mobile.erb'
+      should_assign_to(:section) { @section }
+      should_assign_to(:page)    { @page }
+    end
   end
 end
