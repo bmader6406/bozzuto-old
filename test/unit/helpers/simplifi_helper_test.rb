232ac0e06c800096603eadcb60b8d74bb99b7f00
Chris Jones <chris@yaychris.com>
2013-03-05 17:10:45 -0500

    Add Simpli.fi tracking code to apartment contact TY, resolves #830

---
+++ test/unit/helpers/simplifi_helper_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SimplifiHelperTest < ActionView::TestCase
+  context "#apartment_community_simplifi_code" do
+    should "return the correct html" do
+      code = apartment_contact_simplifi_code
+
+      assert_match /<script async src="http:\/\/i.simpli\.fi\/dpx\.js\?/, code
+      assert_match /<img src="http:\/\/i.simpli.fi\/dpx?/, code
+    end
+  end
+end
