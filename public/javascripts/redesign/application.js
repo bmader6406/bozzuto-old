f787fbddf6bea3900d1ca2b1ef7600a1f6daa4d5
Chris Jones <chris@yaychris.com>
2013-10-17 10:00:39 -0400

    Redesign: Larger Map/Directions link

--- public/javascripts/redesign/application.js
+++ public/javascripts/redesign/application.js
@@ -6,8 +6,16 @@ BOZ.init = function() {
   var controller = BOZ.controller = body.getAttribute('data-controller').toLowerCase();
   var action     = BOZ.action     = body.getAttribute('data-action').toLowerCase();
 
+  BOZ.common.init();
+
   if (controller !== "" && BOZ[controller] && typeof BOZ[controller][action] == "function" ) {
     BOZ[controller][action]();
+  }
+}
+
+BOZ.common = {
+  init: function() {
+    $('[rel="external"]').attr('target', '_blank');
   }
 }
 
