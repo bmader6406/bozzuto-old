d21259d27e6f62d5c348a91f3b0b932b52777ccf
Chris Jones <chris@yaychris.com>
2013-12-10 10:40:05 -0500

    Order leaders and leadership groups by position, resolves #939

--- app/models/leadership_group.rb
+++ app/models/leadership_group.rb
@@ -1,7 +1,9 @@
 class LeadershipGroup < ActiveRecord::Base
   acts_as_list
 
-  has_many :leaders
+  has_many :leaders, :order => 'position ASC'
 
   validates_presence_of :name
+
+  default_scope :order => 'position ASC'
 end
