2146d9cb54eef0733d9181ef1de0855f8c182ea1
Chris Jones <chris@yaychris.com>
2014-03-12 15:20:06 -0400

    Allow command line override of branch when deploying to integration

--- config/deploy/integration.rb
+++ config/deploy/integration.rb
@@ -1,10 +1,10 @@
-set :user, 'www-data'
-set :deploy_to,  "/var/www/bozzuto/integration"
-set :branch, 'origin/master'
+set :user,      'www-data'
+set :deploy_to, '/var/www/bozzuto/integration'
+set :branch,    fetch(:branch, 'origin/master')
 
 set :default_environment, { 'PATH' => '/opt/ruby/bin:$PATH' }
 
-role :web, "bozzuto.integration.vigetx.com"
-role :app, "bozzuto.integration.vigetx.com"
-role :db,  "bozzuto.integration.vigetx.com", :primary => true
+role :web, 'bozzuto.integration.vigetx.com'
+role :app, 'bozzuto.integration.vigetx.com'
+role :db,  'bozzuto.integration.vigetx.com', :primary => true
 
