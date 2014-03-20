c1e2348df53b8ee06cfbfbd6b3b1b04746b732dd
Chris Jones <chris@yaychris.com>
2014-03-20 14:51:05 -0400

    Fix bug with migrations
    One migration had two extra digits in the time stamp. This was causing
    it to always appear as the most recent migration. I renamed that file
    and manually updated the schema_migrations table on integration,
    staging, and production

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2012081313174438) do
+ActiveRecord::Schema.define(:version => 20140224180605) do
 
   create_table "ad_sources", :force => true do |t|
     t.string   "domain_name", :null => false
