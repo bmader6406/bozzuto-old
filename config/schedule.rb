cc930de7b2d97fd66a3330c726ce312e602a0b4b
Chris Jones <chris.jones@viget.com>
2014-06-09 14:17:07 -0400

    Update feed loading schedule, resolves #1027

--- config/schedule.rb
+++ config/schedule.rb
@@ -14,20 +14,11 @@ every 1.day, :at => '7:00 am' do
   rake 'bozzuto:refresh_local_info_feeds'
 end
 
-every 1.day, :at => '7:30 am' do
-  rake 'bozzuto:load_property_link_feed'
-end
-
-every 1.day, :at => '9:00 am' do
-  rake 'bozzuto:load_vaultware_feed'
-end
-
-every 1.day, :at => '2:30 pm' do
-  rake 'bozzuto:load_psi_feed'
-end
-
 every 1.day, :at => ['10:15 am', '2:15 pm'] do
+  rake 'bozzuto:load_vaultware_feed'
+  rake 'bozzuto:load_property_link_feed'
   rake 'bozzuto:load_rent_cafe_feed'
+  rake 'bozzuto:load_psi_feed'
 end
 
 every 1.day, :at => ['5:00 am', '5:00 pm'] do
