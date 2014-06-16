8847bd6b4bfbfd668e5cceb83171d761e3e889f6
Chris Jones <chris.jones@viget.com>
2014-06-16 14:51:56 -0400

    Update integration and staging deploys to use rbenv

--- config/deploy/integration.rb
+++ config/deploy/integration.rb
@@ -2,7 +2,7 @@ set :user,      'www-data'
 set :deploy_to, '/var/www/bozzuto/integration'
 set :branch,    fetch(:branch, 'origin/master')
 
-set :default_environment, { 'PATH' => '/opt/ruby/bin:$PATH' }
+set :default_environment, { 'PATH' => '/opt/rbenv/versions/ree-1.8.7-2012.02/bin:$PATH' }
 
 role :web, 'bozzuto.integration.vigetx.com'
 role :app, 'bozzuto.integration.vigetx.com'
