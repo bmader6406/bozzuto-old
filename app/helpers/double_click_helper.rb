ddfaad4d7e477a3dfe397f7bc5a6151a7e95a2e3
h0tl33t <h0tl33t@gmail.com>
2014-07-15 18:07:53 -0400

    Update DoubleClickHelper to handle new scripts from client

--- app/helpers/double_click_helper.rb
+++ app/helpers/double_click_helper.rb
@@ -1,6 +1,124 @@
 module DoubleClickHelper
+  # apartment_contact_submissions/thank_you.html.erb
+  # apartment_contact_submissions/thank_you.mobile.erb
+  # lasso_submissions/thank_you.mobile.erb
+  # lasso_submissions/thank_you.mobile.erb
   def double_click_community_thank_you_script(community)
-    double_click_floodlight_tag(community.title, 'conta168', 'Contact Info Complete')
+    DoubleClick.new(
+      :name        => community.title,
+      :type        => 'conve135',
+      :cat         => 'conta168',
+      :description => 'Contact Info Complete',
+      :fixed_ord   => true
+    ).floodlight_tag_script
+  end
+
+  # layouts/_head.html.erb (all pages)
+  def double_click_community_landing_script
+    DoubleClick.new(
+      :type        => 'ret01',
+      :cat         => 'land01',
+      :description => 'Landing - Retargeting'
+    ).floodlight_tag_script
+  end
+
+  # apartment_contact_submissions/show.html.erb
+  # apartment_contact_submissions/show.mobile.erb
+  def double_click_community_request_info_script(community)
+    DoubleClick.new(
+      :name        => community.title,
+      :type        => 'conve135',
+      :cat         => 'reque222',
+      :description => 'Request Info'
+    ).floodlight_tag_script
+  end
+
+  # apartment_communities/redesign/_features_and_amenities.html.erb
+  def double_click_community_features_amenities_script(community)
+    DoubleClick.new(
+      :name        => community.title,
+      :type        => 'conve135',
+      :cat         => 'faa01',
+      :description => 'Features and Amenities'
+    ).floodlight_tag_script
+  end
+
+  # community_media/index.html.erb
+  def double_click_community_photos_videos_script(community)
+    DoubleClick.new(
+      :name        => community.title,
+      :type        => 'conve135',
+      :cat         => 'pav01',
+      :description => 'Photos and Videos'
+    ).floodlight_tag_script
+  end
+
+  # apartment_communities/redesign/_floor_plans.html.erb
+  def double_click_community_floor_plans_script(community)
+    DoubleClick.new(
+      :name        => community.title,
+      :type        => 'conve135',
+      :cat         => 'fp01',
+      :description => 'Floor Plans'
+    ).floodlight_tag_script
+  end
+
+  # metros/show.html.erb
+  # metros/show.mobile.erb
+  # areas/show.html.erb (when no amenities filter)
+  # areas/show.mobile.erb (when no amenities filter)
+  # neighborhoods/show.html.erb (when no amenities filter)
+  # neighborhoods/show.mobile.erb (when no amenities filter)
+  # home_neighborhoods/show.html.erb
+  # home_neighborhoods/show.mobile.erb
+  def double_click_neighborhood_view_script(neighborhood)
+    DoubleClick.new(
+      :name        => neighborhood.name,
+      :type        => 'conve135',
+      :cat         => 'nv01',
+      :description => 'Neighborhood View',
+      :image       => true
+    ).floodlight_tag_script
+  end
+
+  # metros/index.html.erb (Find an Apartment page)
+  # metros/index.mobile.erb (Find an Apartment page)
+  # home_neighborhoods/index.html.erb (Find a New Home page)
+  # home_neighborhoods/index.mobile.erb (Find a New Home page)
+  def double_click_select_a_neighborhood_script(type)
+    DoubleClick.new(
+      :name        => type,
+      :type        => 'conve135',
+      :cat         => 'san01',
+      :description => 'Select a Neighborhood'
+    ).floodlight_tag_script
+  end
+
+  # When filterer has a current filter..
+  # neighborhoods/show.html.erb
+  # neighborhoods/show.mobile.erb
+  # areas/show.html.erb
+  # areas/show.mobile.erb
+  def double_click_amenities_filter_script(amenity)
+    DoubleClick.new(
+      :name        => amenity.name,
+      :type        => 'conve135',
+      :cat         => 'af01',
+      :description => 'Amenities Filter',
+      :image       => true
+    ).floodlight_tag_script
+  end
+
+  # email/recently_viewed/thank_you.html.erb
+  def double_click_email_thank_you_script(communities)
+    titles = communities.map(&:title).reject(&:blank?).join(',')
+
+    DoubleClick.new(
+      :name        => titles,
+      :type        => 'conve135',
+      :cat         => 'email947',
+      :description => 'Email Results'
+    ).floodlight_tag_script
   end
 
   def double_click_data_attrs(name, cat)
@@ -10,34 +128,73 @@ module DoubleClickHelper
     }
   end
 
-  def double_click_email_thank_you_script(communities)
-    titles = communities.map(&:title).reject(&:blank?).join(',')
-
-    double_click_floodlight_tag(titles, 'email947', 'Email Results')
-  end
-
   private
 
-  def double_click_floodlight_tag(name, cat, description)
-    encoded_name = URI.encode(name)
+  class DoubleClick < Struct.new(:options)
+    def floodlight_tag_script
+      <<-END.html_safe
+        <!--
+          Start of DoubleClick Floodlight Tag: Please do not remove
+          Activity name of this tag: #{description}
+          URL of the webpage where the tag is expected to be placed: http://www.bozzuto.com
+          This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
+          Creation Date: 07/01/2014
+        -->
 
-    <<-END.html_safe
-      <!--
-      Start of DoubleClick Floodlight Tag: Please do not remove
-      Activity name of this tag: #{description}
-      URL of the webpage where the tag is expected to be placed: http://www.bozzuto.com
-      This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
-      Creation Date: 04/04/2013
-      -->
-      <script type="text/javascript">
-      var axel = Math.random() + "";
-      var a = axel * 10000000000000;
-      document.write('<iframe src="http://4076175.fls.doubleclick.net/activityi;src=4076175;type=conve135;cat=#{cat};u1=#{encoded_name};ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
-      </script>
-      <noscript>
-      <iframe src="http://4076175.fls.doubleclick.net/activityi;src=4076175;type=conve135;cat=#{cat};u1=#{encoded_name}];ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
-      </noscript>
-      <!-- End of DoubleClick Floodlight Tag: Please do not remove -->
-    END
+        <script type="text/javascript">
+          var axel = Math.random() + "";
+          var a = axel * 10000000000000;
+          document.write('<#{tag_type} src="http://4076175.fls.doubleclick.net/#{activity_type};src=4076175;#{type}#{cat}#{u1}ord=1;num=' + a + '?" width="1" height="1" #{tag_ending}');
+        </script>
+
+        <noscript>
+          <#{tag_type} src="http://4076175.fls.doubleclick.net/#{activity_type};src=4076175;#{type}#{cat}#{u1}#{ord}" width="1" height="1" #{tag_ending}
+        </noscript>
+
+        <!-- End of DoubleClick Floodlight Tag: Please do not remove -->
+      END
+    end
+
+    private
+
+    def u1
+      "u1=#{property_name};" if property_name
+    end
+
+    def cat
+      "cat=#{options[:cat]};" if options[:cat]
+    end
+
+    def type
+      "type=#{options[:type]};" if options[:type]
+    end
+
+    def ord
+      options[:fixed_ord] ? "ord=1;num='+ a + '?" : "ord='+ a + '?"
+    end
+
+    def description
+      options[:description]
+    end
+
+    def property_name
+      URI.encode(options[:name]) if options[:name]
+    end
+
+    def image?
+      options[:image]
+    end
+
+    def tag_type
+      image? ? 'img' : 'iframe'
+    end
+
+    def activity_type
+      image? ? 'activity' : 'activityi'
+    end
+
+    def tag_ending
+      image? ? "alt=\"\"/>" : "frameborder=\"0\" style=\"display:none\"></iframe>"
+    end
   end
 end
--- test/unit/helpers/double_click_helper_test.rb
+++ test/unit/helpers/double_click_helper_test.rb
@@ -9,9 +9,128 @@ class DoubleClickHelperTest < ActionView::TestCase
     should "return the correct script" do
       script = double_click_community_thank_you_script(@community)
 
-      assert_match /u1=Yay%20Hooray/, script
+      assert_match /type=conve135/, script
       assert_match /cat=conta168/, script
+      assert_match /u1=Yay%20Hooray/, script
       assert_match /Contact Info Complete/, script
+      assert_match /ord=1;num='\+ a \+ '\?/, script
+    end
+  end
+
+  context "#double_click_community_landing_script" do
+    should "return the correct script" do
+      script = double_click_community_landing_script
+
+      assert_match /type=ret01/, script
+      assert_match /cat=land01/, script
+      assert_match /Landing - Retargeting/, script
+    end
+  end
+
+  context "#double_click_community_request_info_script" do
+    setup do
+      @community = ApartmentCommunity.new(:title => 'Yay Hooray')
+    end
+
+    should "return the correct script" do
+      script = double_click_community_request_info_script(@community)
+
+      assert_match /type=conve135/, script
+      assert_match /cat=reque222/, script
+      assert_match /u1=Yay%20Hooray/, script
+      assert_match /Request Info/, script
+    end
+  end
+
+  context "#double_click_community_features_amenities_script" do
+    setup do
+      @community = ApartmentCommunity.new(:title => 'Yay Hooray')
+    end
+
+    should "return the correct script" do
+      script = double_click_community_features_amenities_script(@community)
+
+      assert_match /type=conve135/, script
+      assert_match /cat=faa01/, script
+      assert_match /u1=Yay%20Hooray/, script
+      assert_match /Features and Amenities/, script
+    end
+  end
+
+  context "#double_click_community_photos_videos_script" do
+    setup do
+      @community = ApartmentCommunity.new(:title => 'Yay Hooray')
+    end
+
+    should "return the correct script" do
+      script = double_click_community_photos_videos_script(@community)
+
+      assert_match /type=conve135/, script
+      assert_match /cat=pav01/, script
+      assert_match /u1=Yay%20Hooray/, script
+      assert_match /Photos and Videos/, script
+    end
+  end
+
+  context "#double_click_community_floor_plans_script" do
+    setup do
+      @community = ApartmentCommunity.new(:title => 'Yay Hooray')
+    end
+
+    should "return the correct script" do
+      script = double_click_community_floor_plans_script(@community)
+
+      assert_match /type=conve135/, script
+      assert_match /cat=fp01/, script
+      assert_match /u1=Yay%20Hooray/, script
+      assert_match /Floor Plans/, script
+    end
+  end
+
+  context "#double_click_neighborhood_view_script" do
+    setup do
+      @neighborhood = Neighborhood.make(:name => 'Yay Hooray')
+    end
+
+    should "return the correct script" do
+      script = double_click_neighborhood_view_script(@neighborhood)
+
+      assert_match /type=conve135/, script
+      assert_match /cat=nv01/, script
+      assert_match /u1=Yay%20Hooray/, script
+      assert_match /Neighborhood View/, script
+      assert_match /<img src/, script
+      assert_match /activity;/, script
+      assert_match /alt=/, script
+    end
+  end
+
+  context "#double_click_select_a_neighborhood_script" do
+    should "return the correct script" do
+      script = double_click_select_a_neighborhood_script('Apartment')
+
+      assert_match /type=conve135/, script
+      assert_match /cat=san01/, script
+      assert_match /u1=Apartment/, script
+      assert_match /Select a Neighborhood/, script
+    end
+  end
+
+  context "#double_click_amenities_filter_script" do
+    setup do
+    @amenity = PropertyFeature.make(:name => 'Rapid Transit')
+    end
+
+    should "return the correct script" do
+      script = double_click_amenities_filter_script(@amenity)
+
+      assert_match /type=conve135/, script
+      assert_match /cat=af01/, script
+      assert_match /u1=Rapid%20Transit/, script
+      assert_match /Amenities Filter/, script
+      assert_match /<img src/, script
+      assert_match /activity;/, script
+      assert_match /alt=/, script
     end
   end
 
