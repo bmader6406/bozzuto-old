bf162d4d4ececc26cfab1df9f2f6ed42008d8504
Chris Jones <chris.jones@viget.com>
2014-07-15 14:06:25 -0400

    Add maintenance page and capistrano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -129,6 +129,32 @@ task :refresh_sitemaps do
   run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec rake sitemap:refresh"
 end
 
+set(:maintenance_source) do
+  File.expand_path(File.dirname(__FILE__) + '/../public/maintenance.html')
+end
+
+set(:maintenance_path)     { "#{shared_path}/system" }
+set(:maintenance_target)   { "#{maintenance_path}/maintenance.html"}
+
+namespace :maintenance do
+  task :default do
+    maintenance.on
+  end
+
+  desc "Enable maintenance mode"
+  task :on, :roles => :web do
+    on_rollback { run "rm -f #{maintenance_target}" }
+
+    page = File.read(fetch(:maintenance_source))
+    put page, fetch(:maintenance_target), :mode => 0644
+  end
+
+  desc "Disable maintenance mode"
+  task :off, :roles => :web do
+    run "rm -f '#{maintenance_target}'"
+  end
+end
+
 Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
   $: << File.join(vendored_notifier, 'lib')
 end
