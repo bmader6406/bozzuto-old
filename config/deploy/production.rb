c26b45ade6d9f4fad9eccfb706fa516cef9041d5
Chris Jones <chris.jones@viget.com>
2014-07-31 12:13:51 -0400

    Deploy production to new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,10 @@
-set :user, 'apache'
-set :deploy_to,  "/var/www/#{application}"
-set :branch, "origin/production"
+set :application, 'bozzuto'
+set :user,        'www-data'
+set :deploy_to,   "/var/www/#{application}"
+set :branch,      "origin/production"
 
-set :default_environment, { 'PATH' => '/opt/ruby-enterprise-1.8.7-2010.02/bin:$PATH' }
+set :default_environment, { 'PATH' => '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH' }
 
-role :web, "bozzuto.com"
-role :app, "bozzuto.com"
-role :db,  "bozzuto.com", :primary => true
+role :web, "54.241.3.36"
+role :app, "54.241.3.36"
+role :db,  "54.241.3.36", :primary => true
