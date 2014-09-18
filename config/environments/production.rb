5e6dfd709aa6bf5125bd84cbc247826e6bdb05fd
Chris Jones <chris.jones@viget.com>
2014-09-18 17:43:15 -0400

    Rails 3 upgrade - add config.ru to the repo
    Also, fix some missing config options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,10 @@ Bozzuto::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.action_controller.consider_all_requests_local = false
-  config.action_controller.perform_caching             = true
+  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = false
+
+  config.active_support.deprecation = :log
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,8 +6,10 @@ Bozzuto::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.action_controller.consider_all_requests_local = false
-  config.action_controller.perform_caching             = true
+  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = false
+
+  config.active_support.deprecation = :log
 
   config.action_mailer.default_url_options = {
     :host => 'bozzuto.staging.vigetx.com'
