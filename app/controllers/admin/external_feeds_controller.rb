acfbbe835533eb0d972612d12008f3656b12d5eb
h0tl33t <h0tl33t@gmail.com>
2014-11-10 15:10:53 -0500

    Add FTP Feed Download section to Apartment Communities sidebar

--- app/controllers/admin/external_feeds_controller.rb
+++ app/controllers/admin/external_feeds_controller.rb
@@ -1,7 +1,8 @@
 class Admin::ExternalFeedsController < Admin::MasterController
   skip_before_filter :set_resource
   skip_before_filter :check_if_user_can_perform_action_on_resource
-  before_filter :create_feed_loader
+  before_filter      :create_feed_loader, :only => :load
+  before_filter      :create_ftp,         :only => :download_feeds
 
   def load
     message = {}
@@ -9,11 +10,11 @@ class Admin::ExternalFeedsController < Admin::MasterController
     name = @loader.feed_name
 
     begin
-      if @loader.can_load_feed?
+      if @loader.can_load?
         @loader.load!
         message[:notice] = "#{name} Feed successfully updated."
 
-      elsif @loader.feed_already_loading?
+      elsif @loader.already_loading?
         message[:notice] = "#{name} Feed is already being updated. Please try again later."
 
       else
@@ -32,6 +33,34 @@ class Admin::ExternalFeedsController < Admin::MasterController
     redirect_to :back, message
   end
 
+  def download_feeds
+    message = {}
+
+    name = @ftp.ftp_name
+
+    begin
+      if @ftp.can_load?
+        @ftp.download_files
+        message[:notice] = "Successfully downloaded feed files via #{name} FTP."
+
+      elsif @ftp.already_loading?
+        message[:notice] = "Feed files are already being downloaded via #{name} FTP. Please try again later."
+
+      else
+        interval = @ftp.load_interval
+        message[:notice] = "Feed files can only be downloaded via #{name} FTP once every #{interval / 3600} hours. Please try again later."
+      end
+
+    rescue Exception => e
+      Rails.logger.debug(e.inspect)
+
+      notify_hoptoad(e)
+
+      message[:alert] = "There was an error downloading the feed files. Please try again later."
+    end
+
+    redirect_to :back, message
+  end
 
   private
 
@@ -40,6 +69,14 @@ class Admin::ExternalFeedsController < Admin::MasterController
       @loader = Bozzuto::ExternalFeed::Loader.loader_for_type(params[:feed_type])
     else
       redirect_to :back, :alert => 'Please specify a feed type.'
+    end
+  end
+
+  def create_ftp
+    if params[:ftp_type].present?
+      @ftp = params[:ftp_type].constantize.new
+    else
+      redirect_to :back, :alert => 'Please specify a feed download type.'
     end
   end
 end
