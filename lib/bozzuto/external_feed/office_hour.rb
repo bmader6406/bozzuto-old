1fda900f81316354839e28bbf1fbe7604f98547f
h0tl33t <h0tl33t@gmail.com>
2015-03-12 14:09:42 -0400

    Use constants from Bozzuto::OfficeHours

--- lib/bozzuto/external_feed/office_hour.rb
+++ lib/bozzuto/external_feed/office_hour.rb
@@ -1,29 +1,6 @@
 module Bozzuto
   module ExternalFeed
     class OfficeHour < Bozzuto::ExternalFeed::FeedObject
-      DAY_MAPPING = {
-        'Sunday'    => 0,
-        'Monday'    => 1,
-        'Tuesday'   => 2,
-        'Wednesday' => 3,
-        'Thursday'  => 4,
-        'Friday'    => 5,
-        'Saturday'  => 6,
-        'Su'        => 0,
-        'M'         => 1,
-        'T'         => 2,
-        'W'         => 3,
-        'Th'        => 4,
-        'F'         => 5,
-        'Sa'        => 6
-      }
-
-      # Covers the formats that show up in the feeds:
-      #   12:00 PM
-      #   08:00:00 AM
-
-      TIME_PARSER = /(?<time>[12]{0,1}\d:\d\d)[:\d\d]*\s*(?<period>AM|PM)/
-
       attr_reader :day,
                   :opens_at,
                   :opens_at_period,
@@ -42,9 +19,9 @@ module Bozzuto
         [attrs.fetch(:opens_at), attrs.fetch(:closes_at)].tap do |(opens_at, closes_at)|
           return if opens_at == 'Closed' || closes_at == 'Closed'
 
-          @day                          = DAY_MAPPING.fetch attrs.fetch(:day)
-          @opens_at, @opens_at_period   = opens_at.match(TIME_PARSER).captures
-          @closes_at, @closes_at_period = closes_at.match(TIME_PARSER).captures
+          @day                          = Bozzuto::OfficeHours::DAY_MAPPING.fetch attrs.fetch(:day)
+          @opens_at, @opens_at_period   = opens_at.match(Bozzuto::OfficeHours::TIME_PARSER).captures
+          @closes_at, @closes_at_period = closes_at.match(Bozzuto::OfficeHours::TIME_PARSER).captures
         end
       end
     end
