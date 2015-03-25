e594d173361114ce950e9707c3e1a16563b4b93d
h0tl33t <h0tl33t@gmail.com>
2015-03-25 11:51:29 -0400

    Create a CoreIdManager to handle the assigning of core ids to existing properties

---
+++ lib/bozzuto/external_feed/core_id_manager.rb
@@ -0,0 +1,61 @@
+module Bozzuto::ExternalFeed
+  class CoreIdManager
+    attr_reader :community
+
+    def self.assign_core_ids!
+      new.assign_core_ids!
+    end
+
+    def self.clear_core_ids!
+      new.clear_core_ids!
+    end
+
+    def initialize(community = nil)
+      @community = community
+    end
+
+    def assign_core_ids!
+      ApartmentCommunity.find_each do |community|
+        if community.core_id.nil?
+          community.update_attributes :core_id => core_id_for(community)
+        end
+      end
+    end
+
+    def clear_core_ids!
+      ApartmentCommunity.update_all(:core_id => nil)
+    end
+
+    def assign_id
+      core_id = if matching_community.try(:core_id).present?
+        matching_community.core_id
+      else
+        community.id
+      end
+
+      community.update_attributes(:core_id => core_id)
+    end
+
+    private
+
+    def core_id_for(community)
+      core_id_map.fetch(community.title)
+    end
+
+    def core_id_map
+      @core_id_map ||= ApartmentCommunity.all.reduce(Hash.new) do |core_id_map, community|
+        if community.core_id.present?
+          core_id_map.merge(community.title => community.core_id)
+        elsif core_id_map[community.title].nil? || (community.included_in_export? && community.published?)
+          core_id_map.merge(community.title => community.id)
+        else
+          core_id_map
+        end
+      end
+    end
+
+    def matching_community
+      ApartmentCommunity.where(:title => community.title).partition(&:included_in_export?).flatten.first
+    end
+  end
+end
---
+++ test/unit/bozzuto/external_feed/core_id_manager_test.rb
@@ -0,0 +1,110 @@
+require 'test_helper'
+
+class Bozzuto::ExternalFeed::CoreIdManagerTest < ActiveSupport::TestCase
+  context "Core ID Manager" do
+    describe ".assign_core_ids!" do
+      context "when a property does not have the same name as any others" do
+        before do
+          @standalone = ApartmentCommunity.make(:title => 'Glitter City')
+        end
+
+        it "assigns the record's id to its core id" do
+          expect {
+            Bozzuto::ExternalFeed::CoreIdManager.assign_core_ids!
+          }.to change { 
+            @standalone.reload.core_id
+          }.from(nil).to(@standalone.id)
+        end
+      end
+
+      context "when there are multiple properties with the same name" do
+        before do
+          @in_export     = ApartmentCommunity.make(:title => 'Boomtown', :included_in_export => true,  :published => false)
+          @not_in_export = ApartmentCommunity.make(:title => 'Boomtown', :included_in_export => false, :published => false)
+          @core          = ApartmentCommunity.make(:title => 'Boomtown', :included_in_export => true,  :published => true)
+        end
+
+        it "assigns all properties a core id equal to that of the first property that's included in the export and published" do
+          Bozzuto::ExternalFeed::CoreIdManager.assign_core_ids!
+
+          @in_export.reload.core_id.should     == @core.id
+          @not_in_export.reload.core_id.should == @core.id
+          @core.reload.core_id.should          == @core.id
+        end
+
+        context "and one of the communities already has a core id" do
+          before do
+            @existing_id = ApartmentCommunity.make(:title => 'Boomtown', :core_id => 123) 
+          end
+
+          it "ensures all communities use the core id" do
+            Bozzuto::ExternalFeed::CoreIdManager.assign_core_ids!
+
+            @in_export.reload.core_id.should     == 123
+            @not_in_export.reload.core_id.should == 123
+            @existing_id.reload.core_id.should   == 123
+          end
+        end
+      end
+    end
+
+    describe ".clear_core_ids!" do
+      before do
+        ApartmentCommunity.make(:core_id => 100)
+        ApartmentCommunity.make(:core_id => 200)
+        ApartmentCommunity.make(:core_id => 300)
+      end
+
+      it "sets all communities' core_ids to nil" do
+        Bozzuto::ExternalFeed::CoreIdManager.clear_core_ids!
+
+        ApartmentCommunity.all.all? { |community| community.core_id.nil? }.should == true
+      end
+    end
+
+    describe "#assign_id" do
+      context "if the community already has a core id" do
+        before do
+          @community = ApartmentCommunity.make(:core_id => 123) 
+        end
+
+        subject { Bozzuto::ExternalFeed::CoreIdManager.new(@community) }
+
+        it "does not change the existing core id" do
+          expect { subject.assign_id }.to_not change { @community.reload.core_id }
+        end
+      end
+
+      context "when there's a community with a matching name that has a core id" do
+        before do
+          ApartmentCommunity.make(:title => 'Boomtown', :core_id => 999, :included_in_export => false)
+          ApartmentCommunity.make(:title => 'Boomtown', :core_id => 123, :included_in_export => true)
+          @community = ApartmentCommunity.make(:title => 'Boomtown', :core_id => nil)
+        end
+
+        subject { Bozzuto::ExternalFeed::CoreIdManager.new(@community) }
+
+        it "assigns that core id to the community" do
+          subject.assign_id
+
+          @community.reload.core_id.should == 123
+        end
+      end
+
+      context "when there's a community with a matching name that does not have a core id" do
+        before do
+          ApartmentCommunity.make(:title => 'Boomtown', :core_id => nil)
+          @community = ApartmentCommunity.make(:title => 'Boomtown', :core_id => nil)
+        end
+
+        subject { Bozzuto::ExternalFeed::CoreIdManager.new(@community) }
+
+        it "assigns the record id as its core id" do
+          subject.assign_id
+
+          @community.reload.core_id.should == @community.id
+        end
+      end
+    end
+  end
+end
