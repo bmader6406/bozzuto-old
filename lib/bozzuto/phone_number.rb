817121438fba3f38118fcc1e770046a32c0b2a29
h0tl33t <h0tl33t@gmail.com>
2015-11-30 15:04:37 -0500

    Refactor the .format method & add a #to_s representation for PhoneNumber

--- lib/bozzuto/phone_number.rb
+++ lib/bozzuto/phone_number.rb
@@ -1,7 +1,19 @@
 module Bozzuto
   class PhoneNumber
+    attr_reader :value
+
     def self.format(input)
-      input.gsub(/[^\d]/, '').last(10)
+      input.to_s.gsub(/[^\d]/, '').last(10) unless input.blank?
+    end
+
+    def initialize(input)
+      @value = self.class.format(input)
+    end
+
+    def to_s
+      match = value.match(/(\d{3})(\d{3})(\d{4})/)
+
+      [match[1], match[2], match[3]].join('.')
     end
   end
 end
--- test/unit/bozzuto/phone_number_test.rb
+++ test/unit/bozzuto/phone_number_test.rb
@@ -32,6 +32,26 @@ class Bozzuto::PhoneNumberTest < ActiveSupport::TestCase
           Bozzuto::PhoneNumber.format(@number).should == '8153813919'
         end
       end
+
+      context "when given nil" do
+        it "returns nil" do
+          Bozzuto::PhoneNumber.format(nil).should == nil
+        end
+      end
+
+      context "when given an empty string" do
+        it "returns nil" do
+          Bozzuto::PhoneNumber.format('').should == nil
+        end
+      end
+    end
+
+    describe "#to_s" do
+      subject { Bozzuto::PhoneNumber.new('8153813919') }
+
+      it "returns a period-separated phone number" do
+        subject.to_s.should == '815.381.3919'
+      end
     end
   end
 end
