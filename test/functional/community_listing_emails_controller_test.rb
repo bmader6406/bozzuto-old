5c88da776829852ec0495bc982b451d9f46f1f23
h0tl33t <h0tl33t@gmail.com>
2016-02-05 07:40:40 -0500

    Fix an email-based integration test

--- test/functional/community_listing_emails_controller_test.rb
+++ test/functional/community_listing_emails_controller_test.rb
@@ -5,7 +5,9 @@ class CommunityListingEmailsControllerTest < ActionController::TestCase
     context "without an email address" do
       setup do
         @community = HomeCommunity.make
+      end
 
+      should "not send an email" do
         expect {
           post :create,
             :home_community_id => @community.id,
@@ -13,10 +15,16 @@ class CommunityListingEmailsControllerTest < ActionController::TestCase
         }.to_not change { ActionMailer::Base.deliveries.count }
       end
 
-      should redirect_to('main community page') { home_community_path(@community) }
+      should "redirect to the main community page" do
+        post :create, :home_community_id => @community.id, :email => ''
 
-      should 'set an error in the flash' do
-        assert flash[:send_listing_errors]
+        response.should redirect_to(home_community_path(@community))
+      end
+
+      should "set an error in the flash" do
+        post :create, :home_community_id => @community.id, :email => ''
+
+        flash[:send_listing_errors].should == true
       end
     end
 
@@ -27,7 +35,7 @@ class CommunityListingEmailsControllerTest < ActionController::TestCase
       end
 
       context "with 'newsletter' unchecked" do
-        setup do
+        should "send an email without signing them up for Bozzuto Buzz" do
           expect {
             expect {
               post :create,
@@ -37,18 +45,21 @@ class CommunityListingEmailsControllerTest < ActionController::TestCase
           }.to_not change { Buzz.count }
         end
 
-        should redirect_to('thank you page') {
-          thank_you_home_community_email_listing_path(@community)
-        }
+        should "redirects to the thank you page" do
+          post :create, :home_community_id => @community.id, :email => @email
 
+          response.should redirect_to(thank_you_home_community_email_listing_path(@community))
+        end
 
-        should 'save the email address in the flash' do
-          assert_equal @email, flash[:send_listing_email]
+        should "save the email address in the flash" do
+          post :create, :home_community_id => @community.id, :email => @email
+
+          flash[:send_listing_email].should == @email
         end
       end
 
       context "with 'newsletter' checked" do
-        setup do
+        should "send an email and sign them up for Bozzuto Buzz" do
           expect {
             expect {
               post :create,
@@ -59,13 +70,23 @@ class CommunityListingEmailsControllerTest < ActionController::TestCase
           }.to change { Buzz.count }.by(1)
         end
 
-        should redirect_to('thank you page') {
-          thank_you_home_community_email_listing_path(@community)
-        }
+        should "redirects to the thank you page" do
+          post :create,
+               :home_community_id => @community.id,
+               :email             => @email,
+               :newsletter        => true
+
+          response.should redirect_to(thank_you_home_community_email_listing_path(@community))
+        end
 
 
-        should 'save the email address in the flash' do
-          assert_equal @email, flash[:send_listing_email]
+        should "saves the email address in the flash" do
+          post :create,
+               :home_community_id => @community.id,
+               :email             => @email,
+               :newsletter        => true
+
+          flash[:send_listing_email].should == @email
         end
       end
     end
@@ -74,21 +95,25 @@ class CommunityListingEmailsControllerTest < ActionController::TestCase
       setup do
         @community = ApartmentCommunity.make
         @email = Faker::Internet.email
+      end
 
+      should "send an email" do
         expect {
-          post :create,
-            :apartment_community_id => @community.id,
-            :email                  => @email
+          post :create, :apartment_community_id => @community.id, :email => @email
         }.to change { ActionMailer::Base.deliveries.count }.by(1)
       end
 
-      should redirect_to('thank you page') {
-        thank_you_apartment_community_email_listing_path(@community)
-      }
+      should "redirects to the thank you page" do
+        post :create, :apartment_community_id => @community.id, :email => @email
+
+        response.should redirect_to(thank_you_apartment_community_email_listing_path(@community))
+      end
 
 
-      should 'save the email address in the flash' do
-        assert_equal @email, flash[:send_listing_email]
+      should "saves the email address in the flash" do
+        post :create, :apartment_community_id => @community.id, :email => @email
+
+        flash[:send_listing_email].should == @email
       end
     end
   end
