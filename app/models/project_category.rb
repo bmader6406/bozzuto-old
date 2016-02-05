96c0ddb5e1beac494ec56ffe3c40f4351aac25e8
h0tl33t <h0tl33t@gmail.com>
2016-02-05 14:58:40 -0500

    Add join_table config to HABTM association

--- app/models/project_category.rb
+++ app/models/project_category.rb
@@ -7,7 +7,7 @@ class ProjectCategory < ActiveRecord::Base
 
   default_scope -> { order(position: :asc) }
 
-  has_and_belongs_to_many :projects
+  has_and_belongs_to_many :projects, join_table: :project_categories_projects
 
   validates_presence_of :title
   validates_uniqueness_of :title
