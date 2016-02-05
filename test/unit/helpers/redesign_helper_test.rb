536d2080009a906b429c108c0b1671c981aefeab
h0tl33t <h0tl33t@gmail.com>
2016-02-05 08:33:11 -0500

    Update output HTML for icon helper

--- test/unit/helpers/redesign_helper_test.rb
+++ test/unit/helpers/redesign_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class RedesignHelperTest < ActionView::TestCase
   context '#icon' do
     should "return the icon html" do
-      assert_equal '<i aria-hidden="true" class="ico">a</i>', icon('a')
+      icon('a').should eq '<i class="ico" aria-hidden="true">a</i>'
     end
   end
 end
