f2d6903e41baad1c06d407a4871bb7f10623bbdd
h0tl33t <h0tl33t@gmail.com>
2016-02-17 13:15:15 -0500

    Add Award to ActiveAdmin

---
+++ app/admin/award.rb
@@ -0,0 +1,41 @@
+ActiveAdmin.register Award do
+  config.sort_order = 'published_at'
+
+  menu parent: 'News & Press'
+
+  permit_params :title,
+                :published,
+                :published_at,
+                :featured,
+                :image,
+                :body,
+                :show_as_featured_news,
+                :home_page_image
+
+  filter :title_or_body_cont, label: 'Search'
+  filter :published_at,       label: 'Published At'
+
+  index do
+    column :title
+    column :featured
+    column :published
+    column :published_at
+
+    actions
+  end
+
+  form do |f|
+    inputs do
+      input :title
+      input :published
+      input :published_at # TODO Datetime picker?
+      input :featured
+      input :image
+      input :body # TODO WYSIWYG
+      input :show_as_featured_news
+      input :home_page_image
+
+      actions
+    end
+  end
+end
