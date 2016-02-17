24530a1ef9f74e9cd35003791f18add24cc5d7bd
Ryan Foster <ryan.foster@viget.com>
2016-02-16 17:22:49 -0700

    Add Publication to ActiveAdmin

---
+++ app/admin/publication.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Publication do
+  menu parent: 'News & Press'
+
+  permit_params :name,
+                :description,
+                :image,
+                :published,
+                :position
+
+  filter :name_cont, label: 'Name'
+
+  index do
+    column :name
+
+    actions
+  end
+
+  form do |f|
+    inputs do
+      input :name
+      input :description
+      input :image
+      input :published
+    end
+
+    actions
+  end
+end
