0710d499ff9aa5c7b3ff6f320b36e0ce330e7f76
Ryan Foster <ryan.foster@viget.com>
2016-02-16 17:52:51 -0700

    Add Video to ActiveAdmin

---
+++ app/admin/video.rb
@@ -0,0 +1,35 @@
+ActiveAdmin.register Video do
+  menu parent: 'Ronin'
+
+  config.filters = false
+
+  permit_params :property,
+                :property_id,
+                :image,
+                :url,
+                :position
+
+  index do
+    column :property
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      rows :id
+      rows :property, :image, :url
+      rows :created_at, :updated_at
+    end
+  end
+
+  form do |f|
+    inputs do
+      input :property
+      input :image
+      input :url
+    end
+
+    actions
+  end
+end
