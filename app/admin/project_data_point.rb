33ebc092aec242bcbb1210c134f9695f732de3ad
Ryan Foster <ryan.foster@viget.com>
2016-02-17 11:21:05 -0700

    Add ProjectDataPoint to ActiveAdmin

---
+++ app/admin/project_data_point.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register ProjectDataPoint do
+  menu parent: 'Ronin',
+       label:  'Project Data Points'
+
+  permit_params :project,
+                :project_id,
+                :name,
+                :data,
+                :position
+
+  filter :name_cont, label: 'Name'
+
+  index do
+    column :project
+    column :name
+    column :data
+
+    actions
+  end
+
+  form do |f|
+    inputs do
+      input :name
+      input :data
+      input :project
+    end
+
+    actions
+  end
+end
