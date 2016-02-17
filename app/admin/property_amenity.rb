932a757c5da98216dc02112f5b7a7ec0c74f7294
Ryan Foster <ryan.foster@viget.com>
2016-02-17 10:47:03 -0700

    Add PropertyAmenity to ActiveAdmin

---
+++ app/admin/property_amenity.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register PropertyAmenity do
+  menu parent: 'Ronin',
+       label:  'Property Amenities'
+
+  permit_params :property,
+                :property_id,
+                :primary_type,
+                :sub_type,
+                :description,
+                :position
+
+  filter :primary_type, as: :select, collection: PropertyAmenity::PRIMARY_TYPE
+
+  index do
+    column :property
+    column :primary_type
+    column :description
+
+    actions
+  end
+
+  form do |f|
+    inputs do
+      input :property
+      input :primary_type, collection: PropertyAmenity::PRIMARY_TYPE
+      input :sub_type,     collection: PropertyAmenity::SUB_TYPE
+      input :description
+    end
+
+    actions
+  end
+end
