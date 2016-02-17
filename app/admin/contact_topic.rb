e6ca4f7bb7c9902f291fd5a070e2e7a116f55fe5
h0tl33t <h0tl33t@gmail.com>
2016-02-17 16:10:09 -0500

    Add ContactTopic to ActiveAdmin

---
+++ app/admin/contact_topic.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register ContactTopic do
+  config.sort_order = 'position_asc'
+
+  menu parent: 'Content'
+  
+  filter :topic_cont, label: 'Topic'
+
+  reorderable
+
+  index as: :reorderable_table do
+    column :topic
+    
+    actions
+  end
+
+  form do |f|
+    inputs do
+      input :topic
+      input :body # TODO WYSIWYG
+      input :recipients
+      input :section
+
+      actions
+    end
+  end
+end
