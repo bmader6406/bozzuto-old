8f6dcbf96d991bd0bd5b56fba73824c3f93bc68e
h0tl33t <h0tl33t@gmail.com>
2016-02-18 17:44:35 -0500

    Add Home placeholder to ActiveAdmin

---
+++ app/admin/home.rb
@@ -0,0 +1,6 @@
+ActiveAdmin.register Home do
+  menu false
+
+  # TODO This looks to be something like a floor plan for an apartment community.
+  # Definitely a nested resource under HomeCommunity.
+end
