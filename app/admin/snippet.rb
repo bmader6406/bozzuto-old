bb8978d9ceb2ed06b6ce4288ab15835d76b00c24
Ryan Foster <ryan.foster@viget.com>
2016-02-22 15:58:33 -0700

    Update Snippet in ActiveAdmin

--- app/admin/snippet.rb
+++ app/admin/snippet.rb
@@ -1,15 +1,32 @@
 ActiveAdmin.register Snippet do
   menu parent: 'Content'
 
+  config.sort_order = 'name_asc'
+
   permit_params :name,
                 :body
 
-  filter :name_cont, label: "Name"
+  filter :name_cont, label: 'Name'
 
   index do
     column :name
 
     actions
+  end
+
+  sidebar :help, only: :index, priority: 0 do
+    'Blocks of text that may appear multiple times throughout the site.'
+  end
+
+  show do
+    attributes_table do
+      rows :id
+      rows :name
+      row  :body do |s|
+        raw s.body
+      end
+      rows :created_at, :updated_at
+    end
   end
 
   form do |f|
