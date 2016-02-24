f0ddfd6e98a8746ea03ee185d140fe1078a528f3
Ryan Foster <ryan.foster@viget.com>
2016-02-24 12:39:05 -0700

    Add Leader to ActiveAdmin

---
+++ app/admin/leader.rb
@@ -0,0 +1,59 @@
+ActiveAdmin.register Leader do
+  menu parent: 'Leaders'
+
+  config.sort_order = "name_asc"
+
+  config.filters = false
+
+  permit_params :name,
+                :title,
+                :company,
+                :bio,
+                :image
+
+  # Work around for models who have overridden `to_param` in AA
+  # See SO issue:
+  # http://stackoverflow.com/questions/7684644/activerecordreadonlyrecord-when-using-activeadmin-and-friendly-id
+  before_filter do
+    Leader.class_eval do
+      def to_param
+        id.to_s
+      end
+    end
+  end
+
+  index do
+    column :name
+    column :title
+    column :company
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :slug
+      row :title
+      row :company
+      row :image do
+        image_tag resource.image.url if resource.image
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    inputs do
+      input :name
+      input :title
+      input :company
+      input :bio
+      input :image, as: :image
+    end
+    
+    actions
+  end
+end
