d7cba652062de0bd11631dde28c8bcbbaa50b109
Ryan Foster <ryan.foster@viget.com>
2016-02-26 14:58:25 -0700

    Update SeoMetadata in ActiveAdmin

--- app/admin/seo_metadata.rb
+++ app/admin/seo_metadata.rb
@@ -1,32 +1,9 @@
 ActiveAdmin.register SeoMetadata do
-  menu parent: 'Ronin'
+  menu false
 
-  permit_params :resource,
-                :resource_id,
+  permit_params :resource_id,
                 :resource_type,
                 :meta_title,
                 :meta_keywords,
                 :meta_description
-
-  filter :meta_title_cont,       label: 'Meta Title'
-  filter :meta_keywords_cont,    label: 'Meta Keywords'
-  filter :meta_description_cont, label: 'Meta Description'
-
-  index do
-    column :meta_title
-    column :meta_keywords
-    column :meta_description
-
-    actions
-  end
-
-  form do |f|
-    inputs do
-      input :meta_title
-      input :meta_keywords
-      input :meta_description
-    end
-
-    actions
-  end
 end
