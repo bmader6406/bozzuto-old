99f71570056ab34615aa2b1c420ea5919c334f01
h0tl33t <h0tl33t@gmail.com>
2016-03-03 16:56:27 -0500

    Add an :extensive time format to show a full-length timestamp

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -3,6 +3,7 @@ Time::DATE_FORMATS[:project_update]  = '%m.%d.%Y.'
 Time::DATE_FORMATS[:home_news]       = '%b.%d.%Y.'
 Time::DATE_FORMATS[:expiration_date] = '%b %d, %Y %H:%M'
 Time::DATE_FORMATS[:tweet]           = '%h %e'
+Time::DATE_FORMATS[:extensive]       = '%B %-d, %Y %l:%M %p'
 
 Date::DATE_FORMATS[:related_project] = '%b. %Y'
 Date::DATE_FORMATS[:month_year] = '%B %Y'
