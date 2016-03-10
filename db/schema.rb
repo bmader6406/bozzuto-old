77b56260e820e8a789eb84c29b570742540dcebd
h0tl33t <h0tl33t@gmail.com>
2016-03-10 09:19:26 -0500

    Copy project-type Property records to Project table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308164614) do
+ActiveRecord::Schema.define(version: 20160310125351) do
 
   create_table "ad_sources", force: :cascade do |t|
     t.string   "domain_name", limit: 255, null: false
