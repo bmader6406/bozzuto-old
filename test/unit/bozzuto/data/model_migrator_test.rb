8e39b680c0c3ad2d4df257cac3e4d31758853fef
h0tl33t <h0tl33t@gmail.com>
2016-03-10 09:16:36 -0500

    Ensure slugs are copied over during model migration

--- test/unit/bozzuto/data/model_migrator_test.rb
+++ test/unit/bozzuto/data/model_migrator_test.rb
@@ -12,14 +12,24 @@ class Bozzuto::Data::ModelMigratorTest < ActiveSupport::TestCase
 
     before do
       @section    = Section.make
-      @property1  = Property.make.tap { |p| p.update_columns(id: 10, type: 'Project', completion_date: Date.today, section_id: @section.id) }
-      @property2  = Property.make.tap { |p| p.update_columns(id: 20, type: 'Project', completion_date: Date.today, section_id: @section.id) }
+      @property1  = Property.make.tap do |p|
+        p.update_columns(id: 10, type: 'Project', completion_date: Date.today, section_id: @section.id)
+        p.send(:create_slug)
+        p.update_attributes(title: 'Slug Test', slug: nil)
+      end
+      @property2  = Property.make.tap do |p|
+        p.update_columns(id: 20, type: 'Project', completion_date: Date.today, section_id: @section.id)
+        p.send(:create_slug)
+      end
       @slideshow1 = PropertySlideshow.make(property: @property1)
       @slideshow2 = PropertySlideshow.make(property: @property2)
     end
 
     describe "#migrate" do
       it "given a set of records of one class, creates records of the target class with all the same attributes and associations" do
+        property1_slugs = @property1.slugs
+        property2_slugs = @property2.slugs
+
         subject.migrate
 
         Project.count.should == 2
@@ -28,10 +38,12 @@ class Bozzuto::Data::ModelMigratorTest < ActiveSupport::TestCase
           project1.id.should        == 10
           project1.section.should   == @section
           project1.slideshow.should == @slideshow1
+          project1.slugs.should match_array(property1_slugs)
 
           project2.id.should        == 20
           project2.section.should   == @section
           project2.slideshow.should == @slideshow2
+          project2.slugs.should match_array(property2_slugs)
         end
 
         @slideshow1.property_type.should == 'Property'
