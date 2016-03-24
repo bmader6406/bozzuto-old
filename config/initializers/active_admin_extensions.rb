a4f55af37a689dc3c5f79af6c3010cd218c4da77
Ryan Foster <ryan.foster@viget.com>
2016-03-24 11:04:48 -0600

    Add View/Delete links to Edit pages in ActiveAdmin

--- config/initializers/active_admin_extensions.rb
+++ config/initializers/active_admin_extensions.rb
@@ -1,3 +1,4 @@
 ActiveAdmin::Views::ActiveAdminForm.send(:include, ActiveAdmin::Reorderable::TableMethods)
 ActiveAdmin::Views::ActiveAdminForm.send(:include, ActiveAdmin::Views::AssociationTable)
 ActiveAdmin::Views::Pages::Show.send(:include, ActiveAdmin::Views::CollectionPanel)
+ActiveAdmin::Resource::ActionItems.send(:include, ActiveAdmin::Resource::DefaultActions)
