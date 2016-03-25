f59857cc4ad9b5d1fcbe92acf3b32ca0eebd2f99
h0tl33t <h0tl33t@gmail.com>
2016-03-25 13:44:23 -0400

    Add Activity Log section to admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,6 +3,28 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
 
+    panel 'Activity Log' do
+      header_action link_to 'View All', [:admin, :changesets]
+
+      table_for Chronolog::Changeset.recent.limit(10) do
+        column 'Date' do |changeset|
+          changeset.created_at.to_date
+        end
+
+        column 'Time' do |changeset|
+          changeset.created_at.strftime("%I:%M %P")
+        end
+
+        column 'Message' do |changeset|
+          change_summary changeset
+        end
+
+        column 'Changes' do |changeset|
+          link_to 'View Changes', admin_changeset_path(changeset)
+        end
+      end
+    end
+
     columns do
       column do
         panel "Recent Buzz Contacts" do
