4b2bfaaf7109852a9aa3e3804db7f7f5a11ab4c9
h0tl33t <h0tl33t@gmail.com>
2016-03-30 15:16:02 -0400

    Strip out Emoji flags from tweet bodies

--- test/unit/emoji_test.rb
+++ test/unit/emoji_test.rb
@@ -3,12 +3,20 @@ require 'test_helper'
 class EmojiTest < ActiveSupport::TestCase
   context "Emoji" do
     describe ".strip!" do
-      subject { "MySQL, y u do dis? \xF0\x9F\x98\x96" }
+      before do
+        @example1 = "MySQL, y u do dis? \xF0\x9F\x98\x96"
+        @example2 = 'Our hearts are with #Brussels today ❤️🇧🇪 #100parkapts #Bozzuto'
+        @example3 = 'Happy Easter from 100 Park! 🌷🐰 #100parkapts #Bozzuto #Easter'
+      end
 
       it "removes emojis from the given string" do
-        Emoji.strip!(subject)
+        Emoji.strip!(@example1)
+        Emoji.strip!(@example2)
+        Emoji.strip!(@example3)
 
-        subject.should == 'MySQL, y u do dis?'
+        @example1.should == 'MySQL, y u do dis?'
+        @example2.should == 'Our hearts are with #Brussels today ️ #100parkapts #Bozzuto'
+        @example3.should == 'Happy Easter from 100 Park!  #100parkapts #Bozzuto #Easter'
       end
     end
   end
