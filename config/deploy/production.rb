63740f1dc97ecbea70ef5d633bb0e5cd45c0811b
h0tl33t <h0tl33t@gmail.com>
2016-04-01 11:41:12 -0400

    Update slack URL

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :user,          'deploy'
 set :branch,        'production'
-set :slack_app_url, 'http://bozzuto.com'
+set :slack_app_url, 'https://www.bozzuto.com'
 
 server '54.215.11.223:8022', :web, :app, :db, :resque_worker, primary: true
 
