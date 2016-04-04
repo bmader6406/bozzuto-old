54cd376f61f5455ab7df1c3e1a9e471b45a7c7ec
h0tl33t <h0tl33t@gmail.com>
2016-04-04 16:58:58 -0400

    Set static date for project default timestamps

--- db/migrate/20160401154142_add_timestamps_to_projects.rb
+++ db/migrate/20160401154142_add_timestamps_to_projects.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToProjects < ActiveRecord::Migration
   def change
-    add_column :projects, :created_at, :datetime, null: false, default: Time.current
-    add_column :projects, :updated_at, :datetime, null: false, default: Time.current
+    add_column :projects, :created_at, :datetime, null: false, default: Time.utc(2016, 4, 1, 15, 45, 39)
+    add_column :projects, :updated_at, :datetime, null: false, default: Time.utc(2016, 4, 1, 15, 45, 39)
   end
 end
