d04639946d479ce020e0f14e261fdc77bd042c76
h0tl33t <h0tl33t@gmail.com>
2016-04-13 15:40:04 -0400

    Fix default style and available styles for redactor-uploaded images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,9 @@
 class Image < ActiveRecord::Base
   has_attached_file :image,
-                    :url             => '/system/:class/:id/photo_:id_:style.:extension',
-                    :styles          => { :resized => '870x375#', :thumb => '55x55#', :mobile => '300>' },
-                    :default_style   => :resized,
-                    :convert_options => { :all => '-quality 80 -strip' }
+                    url:             '/system/:class/:id/photo_:id_:style.:extension',
+                    styles:          { content: '575>', thumb: '100x100', resized: '870x375#', mobile: '300>' },
+                    default_style:   :original,
+                    convert_options: { all: '-quality 80 -strip' }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
