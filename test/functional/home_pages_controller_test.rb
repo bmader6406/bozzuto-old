7ca03ef628378122182a4d829333b5bf7293cb66
Jenny Nguyen <jenny.nguyen@viget.com>
2017-04-11 08:54:35 -0600

    Delete more tests

--- test/functional/home_pages_controller_test.rb
+++ test/functional/home_pages_controller_test.rb
@@ -2,13 +2,10 @@ require 'test_helper'
 
 class HomePagesControllerTest < ActionController::TestCase
   context 'HomePagesController' do
-
     context 'a GET to #index' do
       setup do
         @home_page = HomePage.new
         @home_page.save(:validate => false)
-
-        @about = Section.make(:about)
       end
 
       desktop_device do
@@ -20,7 +17,6 @@ class HomePagesControllerTest < ActionController::TestCase
         should render_with_layout(:homepage)
         should render_template(:index)
         should assign_to(:home_page) { @home_page }
-        should assign_to(:section) { @about }
       end
     end
   end
