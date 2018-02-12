07868d208913e9a3a49079ef9ecfd40fbb4868f7
anild <anild@qburst.com>
2018-02-12 15:46:04 +0530

    mobile page and script for accordion

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -9,6 +9,21 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function () {
+  $('.accordion .tab-content').slideUp();
+    $('.accordion .accordian-heading').click(function(){
+      if($(this).closest('.tab-section').hasClass('active')) {
+        $('.accordion .tab-content').slideUp();
+        $('.accordion .tab-section').removeClass('active');
+      } else {
+        $('.accordion .tab-content').slideUp();
+        $(this).next('.tab-content').slideDown();
+        $('.accordion .tab-section').removeClass('active');
+        $(this).closest('.tab-section').addClass('active');
+      }
+    });
+});
+
 // Mobile Analytics (works with jQuery 1.11.1)
 (function($) {
   window.bozzuto = window.bozzuto || {};
