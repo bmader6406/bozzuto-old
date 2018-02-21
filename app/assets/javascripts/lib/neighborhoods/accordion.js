0dc17ac8e6e5665f0ff73c1d36e33c9e832a1e41
anild <anild@qburst.com>
2018-02-21 12:45:41 +0530

    changes in show more link and accordion js

--- app/assets/javascripts/lib/neighborhoods/accordion.js
+++ app/assets/javascripts/lib/neighborhoods/accordion.js
@@ -1,35 +1,36 @@
 $(document).ready(function () {
 	$('.accordion .tab-content').slideUp();
-    $('.accordion .accordian-heading').click(function(){
-    	if($(this).closest('.tab-section').hasClass('active')) {
-            $('.accordion .tab-content').slideUp();
-            $('.accordion .tab-section').removeClass('active');
-    	} else {
-    		$('.accordion .tab-content').slideUp();
-	    	$(this).next('.tab-content').slideDown();
-    		$('.accordion .tab-section').removeClass('active');
-	    	$(this).closest('.tab-section').addClass('active');
-    	}
-    });
 
-    $('.accordion').each(function(){
- 
-       var LiN = $(this).find('.tab-section').length;
+  $('.accordion .accordian-heading').click(function(){
+  	if($(this).closest('.tab-section').hasClass('active')) {
+      $('.accordion .tab-content').slideUp();
+      $('.accordion .tab-section').removeClass('active');
+  	} else {
+  		$('.accordion .tab-content').slideUp();
+    	$(this).next('.tab-content').slideDown();
+  		$('.accordion .tab-section').removeClass('active');
+    	$(this).closest('.tab-section').addClass('active');
+  	}
+  });
 
-       if( LiN > 3){    
-           $('.tab-section', this).eq(2).nextAll().hide().addClass('toggleable');
-           $(this).append('<div class="more">Show More</div>');    
-       }  
-    });
+  $('.accordion').each(function(){
 
-    $('.accordion').on('click','.more', function(){
-     
-      if( $(this).hasClass('less') ){    
-        $(this).text('Show More').removeClass('less');    
-      }else{
-        $(this).text('Show Less').addClass('less'); 
-      }
+    var LiN = $(this).find('.tab-section').length;
 
-      $(this).siblings('.tab-section.toggleable').slideToggle();
-    }); 
+    if( LiN > 4){    
+      $('.tab-section', this).eq(3).nextAll().hide().addClass('toggleable');
+      $(this).append('<div class="more">Show More</div>');    
+    }  
+  });
+
+  $('.accordion').on('click','.more', function(){
+   
+    if( $(this).hasClass('less') ){    
+      $(this).text('Show More').removeClass('less');    
+    }else{
+      $(this).text('Show Less').addClass('less'); 
+    }
+
+    $(this).siblings('.tab-section.toggleable').slideToggle();
+  }); 
 });
