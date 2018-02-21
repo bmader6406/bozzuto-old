89c0631edeb3a4c49cce6c74dfc3a0a42614c8ca
anild <anild@qburst.com>
2018-02-21 15:11:11 +0530

    adding show more link in mobile view

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -11,17 +11,39 @@ $(document).ready(function() {
 
 $(document).ready(function () {
   $('.accordion .tab-content').slideUp();
-    $('.accordion .accordian-heading').click(function(){
-      if($(this).closest('.tab-section').hasClass('active')) {
-        $('.accordion .tab-content').slideUp();
-        $('.accordion .tab-section').removeClass('active');
-      } else {
-        $('.accordion .tab-content').slideUp();
-        $(this).next('.tab-content').slideDown();
-        $('.accordion .tab-section').removeClass('active');
-        $(this).closest('.tab-section').addClass('active');
-      }
-    });
+
+  $('.accordion .accordian-heading').click(function(){
+    if($(this).closest('.tab-section').hasClass('active')) {
+      $('.accordion .tab-content').slideUp();
+      $('.accordion .tab-section').removeClass('active');
+    } else {
+      $('.accordion .tab-content').slideUp();
+      $(this).next('.tab-content').slideDown();
+      $('.accordion .tab-section').removeClass('active');
+      $(this).closest('.tab-section').addClass('active');
+    }
+  });
+
+  $('.accordion').each(function(){
+    var LiN = $(this).find('.tab-section').length;
+
+    if( LiN > 4){    
+      $('.tab-section', this).eq(3).nextAll().hide().addClass('toggleable');
+      $(this).append('<div class="more">Show More</div>');    
+    } 
+  });
+
+  $('.accordion').on('click','.more', function(){
+     
+    if( $(this).hasClass('less') ){    
+      $(this).text('Show More').removeClass('less');    
+    }else{
+      $(this).text('Show Less').addClass('less'); 
+    }
+
+    $(this).siblings('.tab-section.toggleable').slideToggle();
+   }); 
+  
 });
 
 // Mobile Analytics (works with jQuery 1.11.1)
