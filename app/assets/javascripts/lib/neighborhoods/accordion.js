e6eb6e8a622813e26d735e4d7927e7fd77f37f1c
anild <anild@qburst.com>
2018-02-21 12:24:14 +0530

    adding show more link in the accordion

--- app/assets/javascripts/lib/neighborhoods/accordion.js
+++ app/assets/javascripts/lib/neighborhoods/accordion.js
@@ -11,4 +11,25 @@ $(document).ready(function () {
 	    	$(this).closest('.tab-section').addClass('active');
     	}
     });
+
+    $('.accordion').each(function(){
+ 
+       var LiN = $(this).find('.tab-section').length;
+
+       if( LiN > 3){    
+           $('.tab-section', this).eq(2).nextAll().hide().addClass('toggleable');
+           $(this).append('<div class="more">Show More</div>');    
+       }  
+    });
+
+    $('.accordion').on('click','.more', function(){
+     
+      if( $(this).hasClass('less') ){    
+        $(this).text('Show More').removeClass('less');    
+      }else{
+        $(this).text('Show Less').addClass('less'); 
+      }
+
+      $(this).siblings('.tab-section.toggleable').slideToggle();
+    }); 
 });
